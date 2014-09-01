f6cb8fbcd845a464cdab481dd83117e687023ca7
Ian Chesal <ian.chesal@gmail.com>
2014-08-31 21:57:35 -0700

    Go updates, AWS Java settings

---
+++ zsh/custom/plugins/mygo/mygo.plugin.zsh
@@ -0,0 +1,1 @@
+export GOPATH=/Users/ian/code/go
--- zsh/custom/plugins/myosx/myosx.plugin.zsh
+++ zsh/custom/plugins/myosx/myosx.plugin.zsh
@@ -2,7 +2,7 @@ export JAVA_HOME="$(/usr/libexec/java_home)"
 
 export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.aws/certs/*-pk.pem | /usr/bin/head -1)"
 export EC2_CERT="$(/bin/ls "$HOME"/.aws/certs/*-cert.pem | /usr/bin/head -1)"
-export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/libexec/lib"
+export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.7.1.0/libexec"
 
 [[ -s "$HOME/.aws/keys.zsh" ]] && source "$HOME/.aws/keys.zsh"
 
