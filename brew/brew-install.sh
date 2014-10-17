214d2e91d80b581254d8fcf497e1645ce4174bdd
Ian Chesal <ian.chesal@gmail.com>
2014-10-16 17:23:57 -0700

    Homebrew bootstrap script
    TODO Should probably cask more things
    TODO Move to a Caskfile instead of call `brew cask install` over and
    over

---
+++ brew/brew-install.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+# My regular brew packages...
+brew install autoconf \
+ec2-api-tools \
+glib \
+lame \
+libksba \
+libusb-compat \
+ossp-uuid \
+python \
+unrar \
+automake \
+gdbm \
+gnupg \
+libassuan \
+libnet \
+libyaml \
+packer \
+readline \
+x264 \
+awscli \
+gettext \
+gnupg2 \
+libevent \
+libnids \
+mad \
+pcre \
+sox \
+xz \
+cscope \
+git \
+go \
+libffi \
+libpng \
+mercurial \
+pinentry \
+sqlite \
+zsh \
+ctags \
+git-flow \
+gpg-agent \
+libgcrypt \
+libtool \
+node \
+pkg-config \
+texi2html \
+dirmngr \
+git-utils \
+heroku-toolbelt \
+libgpg-error \
+libusb \
+openssl \
+pth \
+tree
+
+# brew install mysql
+# brew install postgres
+
+# Cask
+brew install caskroom/cask/brew-cask
+brew cask install vagrant
+brew cask install virtualbox
