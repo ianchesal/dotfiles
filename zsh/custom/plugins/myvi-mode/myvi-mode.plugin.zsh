ba1161ab0237cbdae729d3fc2b54eb02e238ad36
Ian Chesal <ian.chesal@gmail.com>
2014-12-13 17:24:05 -0800

    Enable vi mode for zsh because emacs sucks

---
+++ zsh/custom/plugins/myvi-mode/myvi-mode.plugin.zsh
@@ -0,0 +1,8 @@
+bindkey '^P' up-history
+bindkey '^N' down-history
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+bindkey '^r' history-incremental-search-backward
+
+export KEYTIMEOUT=1
