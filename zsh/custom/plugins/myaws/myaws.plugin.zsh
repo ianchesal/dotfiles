58e921137e5ef4afbd978212d413ec71d7ee9b9d
Ian Chesal <ianc@squareup.com>
2015-03-20 15:32:19 -0700

    I don't know what syntax I was writing...
    ...but it sure as hell wasn't zsh. Fixed.

--- zsh/custom/plugins/myaws/myaws.plugin.zsh
+++ zsh/custom/plugins/myaws/myaws.plugin.zsh
@@ -1,6 +1,6 @@
 # My aws customizations
 
 # If we have the Homebrew-installed awscli package, source completions
-if [[ -a /usr/local/share/zsh/site-functions/_aws ]]; do
+if [[ -a /usr/local/share/zsh/site-functions/_aws ]]; then
   source /usr/local/share/zsh/site-functions/_aws
-done
+fi
