eb02d90b02f748dfb5387046f0884866e901a865
Ian Chesal <ianc@squareup.com>
2015-03-20 15:30:57 -0700

    Add command line completion for the `aws` CLI

---
+++ zsh/custom/plugins/myaws/myaws.plugin.zsh
@@ -0,0 +1,6 @@
+# My aws customizations
+
+# If we have the Homebrew-installed awscli package, source completions
+if [[ -a /usr/local/share/zsh/site-functions/_aws ]]; do
+  source /usr/local/share/zsh/site-functions/_aws
+done
