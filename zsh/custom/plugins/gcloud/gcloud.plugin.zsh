8aa4c5d8771c07368383624accdb81465f75aad1
Ian Chesal <ianc@squareup.com>
2017-07-19 16:42:32 -0700

    Support for legacy p12 keyfiles in gcloud

--- zsh/custom/plugins/gcloud/gcloud.plugin.zsh
+++ zsh/custom/plugins/gcloud/gcloud.plugin.zsh
@@ -7,6 +7,10 @@ GCLOUD_SDK_LOCATION="${HOME}/google-cloud-sdk"
 
 export PATH="${GCLOUD_SDK_LOCATION}/bin:$PATH"
 
+# See: https://cloud.google.com/sdk/crypto
+# Required to work with legacy p12 keyfiles
+export CLOUDSDK_PYTHON_SITEPACKAGES=1
+
 if [[ -a "${GCLOUD_SDK_LOCATION}/completion.zsh.inc" ]]; then
   source "${GCLOUD_SDK_LOCATION}/completion.zsh.inc"
 fi
