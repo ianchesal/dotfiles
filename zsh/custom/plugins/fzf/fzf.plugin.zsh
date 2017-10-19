21ac622c67dcd77121462e42956256f9a04f3931
Ian Chesal <ian.chesal@gmail.com>
2017-10-18 21:04:50 -0700

    FZF for zsh and some vimrc tweaks
    FZF for zsh is pretty slick. Ctrl-T gets you file searching that fuzzy
    in your shell. How cool?
    
    Unfortuately I couldn't really get the fzf plugin rolling in neovim so
    it's commented out for now. I also added the gitgutter plugin which is
    nice.

---
+++ zsh/custom/plugins/fzf/fzf.plugin.zsh
@@ -0,0 +1,25 @@
+# Add fzf keybindings to my zsh sessions
+
+# fzf via Homebrew
+if [ -e /usr/local/opt/fzf/shell/completion.zsh ]; then
+  source /usr/local/opt/fzf/shell/key-bindings.zsh
+  source /usr/local/opt/fzf/shell/completion.zsh
+fi
+
+# fzf via local installation
+if [ -e ~/.fzf ]; then
+  _append_to_path ~/.fzf/bin
+  source ~/.fzf/shell/key-bindings.zsh
+  source ~/.fzf/shell/completion.zsh
+fi
+
+# fzf + ag configuration
+if _has fzf && _has ag; then
+  export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+  export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
+  export FZF_DEFAULT_OPTS='
+  --color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108
+  --color info:108,prompt:109,spinner:108,pointer:168,marker:168
+  '
+fi
