5b7f145b386f3d89677f1bf302f1cf35ca035a73
Ian Chesal <ian.chesal@gmail.com>
2018-01-01 16:07:58 -0800

    Remove all the oh-my-zsh custom work
    I no longer need all the custom theme and plugin stuff for oh-my-zsh.

--- zsh/custom/plugins/mygit/mygit.plugin.zsh
+++
@@ -1,7 +0,0 @@
-# My git extensions
-alias gci='git commit --verbose'
-alias gpoh='git push origin HEAD'
-alias gss='git status --short --branch'
-alias v='vim'
-alias gdl='git diff --color=always | less -r'
-alias gll='git log --color=always | less -r'
--- zsh/custom/plugins/myruby/myruby.plugin.zsh
+++
@@ -1,5 +0,0 @@
-# My ruby extensions
-alias brake='bundle exec rake'
-alias ber='bundle exec ruby'
-alias be='bundle exec'
-alias brails='bundle exec rails'
