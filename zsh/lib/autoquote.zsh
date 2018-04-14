bad916352d4ba932830ba74682ee489d73ac9c18
Ian Chesal <ian.chesal@gmail.com>
2018-04-14 15:52:49 -0700

    Better URL auto-quoting. iTerm2 integration.
    It wasn't working correctly before. Now it is.
    
    iTerm2 integration is experimental. May not keep it.

--- zsh/lib/autoquote.zsh
+++ zsh/lib/autoquote.zsh
@@ -1,5 +1,17 @@
-# autoquote metacharacters in urls
-autoload -U url-quote-magic
-zle -N self-insert url-quote-magic
-# make sure to escape * in scp commands
-zstyle -e :urlglobber url-other-schema '[[ $words[1] == scp ]] && reply=("*") || reply=(http https ftp)'
+# bracketed-paste-magic is known buggy in zsh 5.1.1 (only), so skip it there; see #4434
+autoload -Uz is-at-least
+if [[ $ZSH_VERSION != 5.1.1 ]]; then
+  for d in $fpath; do
+  	if [[ -e "$d/url-quote-magic" ]]; then
+  		if is-at-least 5.1; then
+  			autoload -Uz bracketed-paste-magic
+  			zle -N bracketed-paste bracketed-paste-magic
+  		fi
+  		autoload -Uz url-quote-magic
+  		zle -N self-insert url-quote-magic
+      # make sure to escape * in scp commands
+      zstyle -e :urlglobber url-other-schema '[[ $words[1] == scp ]] && reply=("*") || reply=(http https ftp)'
+      break
+  	fi
+  done
+fi
