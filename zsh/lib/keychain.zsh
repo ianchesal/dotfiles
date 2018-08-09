093f3b818c3fd76e40be2f51d1b99760808eb241
Ian Chesal <irc@stripe.com>
2018-08-09 11:18:02 -0700

    Prep work for new work environment support
    New job, new environment challenges! Setup for rbenv and make sure
    keychain continues to work at home but don't do this setup in my work
    environment.

--- zsh/lib/keychain.zsh
+++ zsh/lib/keychain.zsh
@@ -1,6 +1,6 @@
 # For keychain frontend to ssh-agent
 # See: brew info keychain
-if _has keychain; then
+if _has keychain && [ -f "~/.ssh/id_rsa" ]; then
   export SSH_KEY_PATH="~/.ssh/id_rsa"
   # funtoo keychain
   eval `keychain -q --eval ~/.ssh/id_rsa`
