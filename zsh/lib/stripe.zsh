40e418916a766302cfa87acb65fb4bbd8f3b8c0f
Ian Chesal <ianc@stripe.com>
2018-08-14 14:05:12 -0700

    Stripe environment
    This is what I've found I've needed thus far.

---
+++ zsh/lib/stripe.zsh
@@ -0,0 +1,14 @@
+autoload -U +X bashcompinit && bashcompinit
+complete -C /Users/ianc/stripe/space-commander/bin/commands/sc-complete sc
+complete -C /Users/ianc/stripe/space-commander/bin/commands/sc-complete _sc
+
+[ -f ~/.stripe-repos.sh ] && source ~/.stripe-repos.sh
+
+path+="/Users/ianc/stripe/password-vault/bin"
+path+="/Users/ianc/stripe/space-commander/bin"
+
+### BEGIN HENSON
+path+="/Users/ianc/stripe/henson/bin"
+### END HENSON
+
+export PATH
