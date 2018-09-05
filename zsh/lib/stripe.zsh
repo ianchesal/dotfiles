5171b599642e1a79749c73fbc4d119b6d75145f1
Ian Chesal <ianc@stripe.com>
2018-09-05 11:31:21 -0700

    Box stripe zsh config to just stripe machines
    And add alias to stripe-git so git is instrumented for debugging
    purposes.

--- zsh/lib/stripe.zsh
+++ zsh/lib/stripe.zsh
@@ -1,17 +1,20 @@
-autoload -U +X bashcompinit && bashcompinit
-complete -C /Users/ianc/stripe/space-commander/bin/commands/sc-complete sc
-complete -C /Users/ianc/stripe/space-commander/bin/commands/sc-complete _sc
+if [[ -d "$HOME/stripe" ]]; then
+  autoload -U +X bashcompinit && bashcompinit
+  complete -C /Users/ianc/stripe/space-commander/bin/commands/sc-complete sc
+  complete -C /Users/ianc/stripe/space-commander/bin/commands/sc-complete _sc
 
-[ -f ~/.stripe-repos.sh ] && source ~/.stripe-repos.sh
+  [ -f ~/.stripe-repos.sh ] && source ~/.stripe-repos.sh
 
-path+="/Users/ianc/stripe/password-vault/bin"
-path+="/Users/ianc/stripe/space-commander/bin"
+  path+="/Users/ianc/stripe/password-vault/bin"
+  path+="/Users/ianc/stripe/space-commander/bin"
 
-### BEGIN HENSON
-path+="/Users/ianc/stripe/henson/bin"
-### END HENSON
+  ### BEGIN HENSON
+  path+="/Users/ianc/stripe/henson/bin"
+  ### END HENSON
 
-# Useful stripe aliases and functions
-alias stripe-curl='curl -s --unix-socket ~/.stripeproxy'
+  # Useful stripe aliases and functions
+  alias stripe-curl='curl -s --unix-socket ~/.stripeproxy'
+  alias git=stripe-git
 
-export PATH
+  export PATH
+fi
