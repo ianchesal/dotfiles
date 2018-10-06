30ef10dcece1093cc1bdde54be82b32c1502932d
Ian Chesal <ian.chesal@gmail.com>
2018-10-05 20:17:58 -0700

    Better Fisher bootstrapping

--- fish/config.fish
+++ fish/config.fish
@@ -2,26 +2,44 @@
 # I've found https://github.com/jorgebucaran/fish-shell-cookbook incredibly
 # useful for guiding my setup here.
 
-# BOOTSTRAP
-# Bootstrap Fisherman if it's not present
-# See: https://github.com/fisherman/fisherman
-if not functions -q fisher
-  echo "Installing fisher for the first time..." >&2
-  set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-  curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-  fisher
-  curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-  # Now install all the fisherman plugins I like:
-  fisher add \
-    daenney/rbenv \
-    rafaelrinaldi/pure
-end
-
 function __append_to_path
   if test -d $argv[1]
     if not contains $argv[1] $fish_user_paths
       set -U fish_user_paths $argv[1] $fish_user_paths
     end
+  end
+end
+
+function __read_confirm
+  set confirm 'x'
+  if [ -z "$argv" ]; # No arguments
+    set message 'Do you want to continue? [y/N] '
+  else
+    set message "$argv "
+  end
+  while true
+    read -l -P $message confirm
+    switch $confirm
+      case Y y
+        return 0
+      case '' N n
+        return 1
+    end
+  end
+end
+
+# Bootstrap Fisherman if it's not present
+# See: https://github.com/jorgebucaran/fisher
+if not functions -q fisher
+  if __read_confirm "fisher is not installed -- install fisher now? [y/N] "
+    echo "Installing fisher..." >&2
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    # Now install all the fisherman plugins I like:
+    echo "You need to run:"
+    echo fisher add \
+      daenney/rbenv \
+      rafaelrinaldi/pure
   end
 end
 
