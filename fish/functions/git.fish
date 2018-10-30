6fdca6406869ea33bad78f33ddfadbc00bc04765
Ian Chesal <ianc@stripe.com>
2018-10-30 13:07:34 -0700

    Add support for github hub
    hub is the future. embrace it.

--- fish/functions/git.fish
+++ fish/functions/git.fish
@@ -1,7 +1,11 @@
 function git
-  if test -x /usr/local/bin/stripe-git
-    command stripe-git $argv
+  if type -q hub
+    command hub $argv
   else
-    command git $argv
+    if test -x /usr/local/bin/stripe-git
+      command stripe-git $argv
+    else
+     command git $argv
+    end
   end
 end
