b4bdc8d68c43f481890c4ae4e1aaeca50b98b1c6
Ian Chesal <ianc@stripe.com>
2019-07-15 17:20:44 -0700

    fzf and ripgrep refinements

--- zsh/lib/fzf.zsh
+++ zsh/lib/fzf.zsh
@@ -18,7 +18,6 @@ fi
 # fzf configuration
 if _has fzf; then
   export FZF_DEFAULT_OPTS='
-  --color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108
   --color info:108,prompt:109,spinner:108,pointer:168,marker:168
   --preview "[[ $(file --mime {}) =~ binary ]] &&
              echo {} is a binary file ||
@@ -26,9 +25,9 @@ if _has fzf; then
              cat {}) 2> /dev/null | head -$LINES"'
 fi
 
-# fzf + ag configuration
-if _has fzf && _has ag; then
-  export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
+# fzf + ripgrep configuration
+if _has rg; then
+  export FZF_DEFAULT_COMMAND='rg --files --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
   export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
 fi
