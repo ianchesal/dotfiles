50e534be029540334ac0d80c1e4916bdc0c77092
Ian Chesal <ian.chesal@gmail.com>
2019-07-17 19:57:37 -0700

    FZF integrations with EVERYTHING in zsh!
    This honestly feels better than fish to me. Fish was just too weird.

--- zsh/lib/homebrew.zsh
+++ zsh/lib/homebrew.zsh
@@ -8,3 +8,39 @@ export HOMEBREW_NO_ANALYTICS=1
 
 # Cask options
 HOMEBREW_CASK_OPTS="--appdir=~/Applications"
+
+if _has fzf; then
+# Install (one or multiple) selected application(s)
+# using "brew search" as source input
+# mnemonic [B]rew [I]nstall [P]lugin
+bip() {
+	local inst=$(brew search | fzf -m)
+
+	if [[ $inst ]]; then
+		for prog in $(echo $inst);
+		do; brew install $prog; done;
+	fi
+}
+
+# Update (one or multiple) selected application(s)
+# mnemonic [B]rew [U]pdate [P]lugin
+bup() {
+	local upd=$(brew leaves | fzf -m)
+
+	if [[ $upd ]]; then
+		for prog in $(echo $upd);
+		do; brew upgrade $prog; done;
+	fi
+}
+
+# Delete (one or multiple) selected application(s)
+# mnemonic [B]rew [C]lean [P]lugin (e.g. uninstall)
+bcp() {
+	local uninst=$(brew leaves | fzf -m)
+
+	if [[ $uninst ]]; then
+		for prog in $(echo $uninst);
+		do; brew uninstall $prog; done;
+	fi
+}
+fi
