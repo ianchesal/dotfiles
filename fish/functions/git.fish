a6530585541858fc3c6c785d856816d844fcd79d
Ian Chesal <ian.chesal@gmail.com>
2019-09-01 21:23:28 -0700

    👋 Bye Stripe

--- fish/functions/git.fish
+++ fish/functions/git.fish
@@ -2,10 +2,6 @@ function git
   if type -q hub
     command hub $argv
   else
-    if test -x /usr/local/bin/stripe-git
-      command stripe-git $argv
-    else
-     command git $argv
-    end
+    command git $argv
   end
 end
