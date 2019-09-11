1337ed7fc66cfe43fbf4d0348cb47599af85ad6e
Ian Chesal <ian.chesal@gmail.com>
2019-09-11 14:11:10 -0700

    Use the gcloud plugin for oh-my-zsh I made

--- zsh/lib/gcloud.zsh
+++ zsh/lib/gcloud.zsh
@@ -1,23 +1,3 @@
-# Look for the GCloud installation
-if [[  -d "${HOME}/google-cloud-sdk" ]]; then
-  export CLOUD_SDK_LOCATION="${HOME}/google-cloud-sdk"
-elif [[ -d "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk" ]]; then
-  export CLOUD_SDK_LOCATION="/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
-elif [[ -d "/usr/share/google-cloud-sdk" ]]; then
-  export CLOUD_SDK_LOCATION="/usr/share/google-cloud-sdk"
-elif [[ -d "/snap/google-cloud-sdk/current" ]]; then
-  export CLOUD_SDK_LOCATION="/snap/google-cloud-sdk/current"
-fi
-
-
-if (( ${+CLOUD_SDK_LOCATION} )); then
-  if ! type "gcloud" > /dev/null; then
-    # Only source this if GCloud isn't already on the path
-    source "${CLOUD_SDK_LOCATION}/path.zsh.inc"
-  fi
-  source "${CLOUD_SDK_LOCATION}/completion.zsh.inc"
-fi
-
 # Required for gcloud SDK to work in my environment
 # because I don't have python2 in the path any more.
 # This was set up with:
