3ca389ef598021bfb9d2c0dee52b269f47af029d
Ian Chesal <ian.chesal@gmail.com>
2020-02-07 15:34:31 -0800

    Docker support

---
+++ zsh/lib/docker.zsh
@@ -0,0 +1,2 @@
+alias docker_clean_images='docker rmi $(docker images -a --filter=dangling=true -q)'
+alias docker_clean_ps='docker rm $(docker ps --filter=status=exited --filter=status=created -q)'
