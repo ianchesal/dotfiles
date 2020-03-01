46631c680a291904669c8fa2262d0f2fbef9b7d5
Ian Chesal <ian.chesal@gmail.com>
2020-03-01 11:01:10 -0800

    Migrate everything into zshrc
    So it turns out loading environment from file is slowing down my new
    shell startup time by a fairly significant amount of time. I'm migrating
    it all into zshrc so it's faster.

--- zsh/lib/homebrew.zsh
+++ zsh/lib/homebrew.zsh
@@ -1,14 +1,12 @@
-if [ "$(uname 2> /dev/null)" = "Darwin" ]; then
+if type "brew" > /dev/null; then
+  # For the 5.0.x Homebrew'ed zsh installation...
+  # unalias run-help
+  autoload run-help
+  HELPDIR=/usr/local/share/zsh/help
 
-# For the 5.0.x Homebrew'ed zsh installation...
-# unalias run-help
-autoload run-help
-HELPDIR=/usr/local/share/zsh/help
+  # Opt out of Homebrew analytics
+  export HOMEBREW_NO_ANALYTICS=1
 
-# Opt out of Homebrew analytics
-export HOMEBREW_NO_ANALYTICS=1
-
-# Cask options
-HOMEBREW_CASK_OPTS="--appdir=~/Applications"
-
+  # Cask options
+  HOMEBREW_CASK_OPTS="--appdir=~/Applications"
 fi
