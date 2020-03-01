81173e6be3c29fd7eb0519fe52d869f5c5f7d930
Ian Chesal <ian.chesal@gmail.com>
2020-03-01 10:32:55 -0800

    zsh syntax highlighting
    Like fish. But it's not fish, so it's better.

---
+++ zsh/lib/syntax-highlighting.zsh
@@ -0,0 +1,4 @@
+if [ -f /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
+  source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+  export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/usr/local/share/zsh-syntax-highlighting/highlighters
+fi
