d0b053358124b142484f69d56af828ac30b9ba19
Ian Chesal <ian.chesal@gmail.com>
2022-04-17 22:19:21 -0400

    Use Brewfile for homebrew re-installation

--- brew/brew-install.sh
+++ brew/brew-install.sh
@@ -1,47 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# My regular brew packages...
-brew install \
-  aspell \
-  awscli \
-  bash-completion \
-  emacs \
-  ffmpeg \
-  fish \
-  fzf \
-  git \
-  git-delta \
-  jq \
-  keychain \
-  mas \
-  neovim \
-  node \
-  packer \
-  packer-completion \
-  perl \
-  pyenv \
-  pyenv-virtualenv \
-  rbenv \
-  ripgrep \
-  ruby-build \
-  terminal-notifier \
-  tmux \
-  tree \
-  yarn \
-  youtube-dl \
-  zsh \
-  zsh-syntax-highlighting
-
-# brew install mysql
-# brew install postgres
-
-# Cask
-brew install caskroom/cask/brew-cask
-brew cask install vagrant
-brew cask install virtualbox
-brew cask install kaleidoscope
-brew cask install xact
-brew cask install jdownloader
-#brew cask install google-cloud-sdk
-#brew cask install handbrake
-brew cask install 1password-cli
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew tap Homebrew/bundle
+cd "$(dirname "$0")"
+brew bundle
