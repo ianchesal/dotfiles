d6fcf87b5cd8742263128698def50befe504bb6e
Ian Chesal <ian.chesal@gmail.com>
2022-08-07 14:34:41 -0400

    Fix broken python initialization
    Double init'ed python stuff and some of it was broken. This smooths
    things out so it works nicely.

--- zsh/zshrc.d/python.zsh
+++ zsh/zshrc.d/python.zsh
@@ -1,6 +1,43 @@
 command -v pyenv &> /dev/null && FOUND_PYENV=1 || FOUND_PYENV=0
 if [[ $FOUND_PYENV -eq 1 ]]; then
   export PYENV_ROOT=$(pyenv root)
+else
+  export PYENV_ROOT="$HOME/.pyenv"
 fi
 unset FOUND_PYENV
 export PIPENV_IGNORE_VIRTUALENVS=1
+
+if ! _has pyenv; then
+  add_to_path "$PYENV_ROOT/bin"
+fi
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+
+# Pipenv completion
+_pipenv() {
+  eval $(env COMMANDLINE="${words[1,$CURRENT]}" _PIPENV_COMPLETE=complete-zsh pipenv)
+}
+compdef _pipenv pipenv
+
+# Automatic pipenv shell activation/deactivation
+_togglePipenvShell() {
+  # deactivate shell if Pipfile doesn't exist and not in a subdir
+  if [[ ! -f "$PWD/Pipfile" ]]; then
+    if [[ "$PIPENV_ACTIVE" == 1 ]]; then
+      if [[ "$PWD" != "$pipfile_dir"* ]]; then
+        exit
+      fi
+    fi
+  fi
+
+  # activate the shell if Pipfile exists
+  if [[ "$PIPENV_ACTIVE" != 1 ]]; then
+    if [[ -f "$PWD/Pipfile" ]]; then
+      export pipfile_dir="$PWD"
+      pipenv shell
+    fi
+  fi
+}
+autoload -U add-zsh-hook
+add-zsh-hook chpwd _togglePipenvShell
+_togglePipenvShell
