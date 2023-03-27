16551b78d3f82c5a90498aff04568c655876b9de
Ian Chesal <ian.chesal@gmail.com>
2023-03-27 10:21:59 -0400

    s/fzf/Telescope/
    Holy hell Telescope is awesome. Using that overlay stuff in NeoVIM makes
    the UI here waaaaaay nicer. And it's freaking fast too.

--- vim/lua/config.lua
+++ vim/lua/config.lua
@@ -1,3 +1,11 @@
+-- Telescope configuration
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.command_history, {})
+vim.keymap.set('n', '<leader>fm', builtin.marks, {})
+
 -- prevent typo when pressing `wq` or `q`
 vim.cmd [[
 cnoreabbrev <expr> W ((getcmdtype() is# ':' && getcmdline() is# 'W')?('w'):('W'))
