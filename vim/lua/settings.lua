d5c21513ff533fa116a3245226cc3609bd0ba9e0
Ian Chesal <ian.chesal@gmail.com>
2023-03-27 10:49:28 -0400

    A step towards a lua-based NeoVIM configuration
    Can't fight the future, right?

---
+++ vim/lua/settings.lua
@@ -0,0 +1,8 @@
+-- prevent typo when pressing `wq` or `q`
+vim.cmd [[
+cnoreabbrev <expr> W ((getcmdtype() is# ':' && getcmdline() is# 'W')?('w'):('W'))
+cnoreabbrev <expr> Q ((getcmdtype() is# ':' && getcmdline() is# 'Q')?('q'):('Q'))
+cnoreabbrev <expr> WQ ((getcmdtype() is# ':' && getcmdline() is# 'WQ')?('wq'):('WQ'))
+cnoreabbrev <expr> Wq ((getcmdtype() is# ':' && getcmdline() is# 'Wq')?('wq'):('Wq'))
+]]
+
