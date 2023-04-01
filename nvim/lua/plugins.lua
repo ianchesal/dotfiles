b94f6c014f2a819b76beb850cb5e90389bd83b71
Ian Chesal <ian.chesal@gmail.com>
2023-04-01 18:47:08 -0400

    It's getting there...
    I still don't have Deoplete-like things happening. And endwise for
    treesitter refuses to load for some reason.
    
    I'm missing a few other things like reopening files where I left off and
    what not.
    
    I'll keep banging away at it.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -14,23 +14,99 @@ local packer_bootstrap = ensure_packer()
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'arcticicestudio/nord-vim'
-  use {'nvim-telescope/telescope.nvim', requires = {'nvim-lua/plenary.nvim'}}
-  use 'nvim-treesitter/nvim-treesitter'
-  use {
-	  'nvim-tree/nvim-tree.lua',
-	  requires = {
-      'nvim-tree/nvim-web-devicons',
-    },
-	  config = function()
-      require'nvim-tree'.setup {}
-    end
-  }
   use 'tpope/vim-fugitive'
   use 'tpope/vim-surround'
-  use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'}}
+
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = 'nvim-lua/plenary.nvim',
+    config = function()
+      require('config.telescope').setup()
+    end,
+  }
+
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = "all",
+        highlight = {
+          enable = true,
+        },
+        additional_vim_regex_highlighting = false,
+        autoinstall = true,
+        highlight = { enable = true },
+        endwise = { enable = true },
+        indent = { enable = true },
+      }
+
+      -- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      -- parser_config.just = {
+      --   install_info = {
+      --     url = "https://github.com/IndianBoy42/tree-sitter-just", -- local path or git repo
+      --     files = { "src/parser.c", "src/scanner.cc" },
+      --     branch = "main",
+      --   },
+      --   maintainers = { "@IndianBoy42" },
+      -- }
+    end,
+  }
+
+  use {
+    'windwp/nvim-autopairs',
+    requires = 'nvim-treesitter',
+    -- module = { 'nvim-autopairs.completion.cmp', 'nvim-autopairs' },
+    config = function()
+      require('config.autopairs').setup()
+    end,
+  }
+
+  use {
+    'RRethy/nvim-treesitter-endwise',
+    requires = 'nvim-treesitter',
+    event = 'InsertEnter',
+    disable = false,
+  }
+
+  use {
+    "nathom/filetype.nvim",
+    config = function()
+      require("filetype").setup {
+        overrides = {
+          extensions = {
+            tf = "terraform",
+            tfvars = "terraform",
+            tfstate = "json",
+          }
+        }
+      }
+    end,
+  }
+
+  use {
+	  'nvim-tree/nvim-tree.lua',
+	  requires = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require("config.nvimtree").setup()
+    end,
+  }
+
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = {'nvim-lua/plenary.nvim'},
+    config = function()
+      require("config.gitsigns").setup()
+    end,
+  }
+
   use {
     'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    after = 'nvim-treesitter',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+    config = function()
+      require("config.lualine").setup()
+    end,
   }
 
   -- Automatically set up your configuration after cloning packer.nvim
