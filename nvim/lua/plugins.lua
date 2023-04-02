0b3380d7e9978fa1dbb500ea86c412b3a2463155
Ian Chesal <ian.chesal@gmail.com>
2023-04-02 19:22:49 -0400

    TSUpdate in the Packer block was giving me issues on init

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -27,7 +27,7 @@ return require('packer').startup(function(use)
 
   use {
     "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
+    -- run = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup {
         ensure_installed = "all",
