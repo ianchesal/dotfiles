9e63e98f41d15a397274fb413e7dc62d27d59fe9
Ian Chesal <ian@withpersona.com>
2023-04-03 13:45:12 -0400

    <leader>x works fine for deleting buffers in NV Chad

--- nvim/custom/mappings.lua
+++ nvim/custom/mappings.lua
@@ -10,7 +10,7 @@ M.disabled = {
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<leader>bq"] = {"<cmd> Bdelete <CR>", "Delete buffer", opts = { noremap = true } },
+    -- ["<leader>bq"] = {"<cmd> Bdelete <CR>", "Delete buffer", opts = { noremap = true } }, -- Use <leader>x in NV Chad
     ["<leader>gg"] = {"<cmd> Git <CR>", "git fugitive", opts = { noremap = true } },
   },
 }
