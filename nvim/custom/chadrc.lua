46ccd665e25a3206115567d354ac3a8205bf1543
Ian Chesal <ian@withpersona.com>
2023-04-03 18:15:51 -0400

    Some Chad UI configs
    Going with tokyonight for now and making sure the edges on my bars
    aren't anything by squared off.

--- nvim/custom/chadrc.lua
+++ nvim/custom/chadrc.lua
@@ -2,25 +2,25 @@
 local M = {}
 
 -- Path to overriding theme and highlights files
-local highlights = require "custom.highlights"
+local highlights = require("custom.highlights")
 
 M.ui = {
-  theme = "tokyodark",
-  theme_toggle = { "tokyonight", "tokyodark" },
+	theme = "tokyonight",
+	theme_toggle = { "tokyonight", "tokyodark" },
 
-  hl_override = highlights.override,
-  hl_add = highlights.add,
-  statusline = {
-    separator_style = "block",
-  },
-  nvdash = {
-    load_on_startup = true,
-  },
+	hl_override = highlights.override,
+	hl_add = highlights.add,
+	statusline = {
+		separator_style = "block",
+	},
+	nvdash = {
+		load_on_startup = true,
+	},
 }
 
 M.plugins = "custom.plugins"
 
 -- check core.mappings for table structure
-M.mappings = require "custom.mappings"
+M.mappings = require("custom.mappings")
 
 return M
