95947417547a561fe863c3cb330bca1073dd7feb
Ian Chesal <ian.chesal@gmail.com>
2023-04-03 21:33:01 -0400

    Allow <leader>b
    With <leader>x to close, the b thing actually makes sense. I'll roll
    with it.

--- nvim/custom/mappings.lua
+++ nvim/custom/mappings.lua
@@ -2,17 +2,17 @@
 local M = {}
 
 M.disabled = {
-  n = {
-    ['<leader>b'] = '',
-  }
+	n = {
+		-- ['<leader>b'] = '',
+	},
 }
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    -- ["<leader>bq"] = {"<cmd> Bdelete <CR>", "Delete buffer", opts = { noremap = true } }, -- Use <leader>x in NV Chad
-    ["<leader>gg"] = {"<cmd> Git <CR>", "git fugitive", opts = { noremap = true } },
-  },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+		-- ["<leader>bq"] = {"<cmd> Bdelete <CR>", "Delete buffer", opts = { noremap = true } }, -- Use <leader>x in NV Chad
+		["<leader>gg"] = { "<cmd> Git <CR>", "git fugitive", opts = { noremap = true } },
+	},
 }
 
 -- more keybinds!
