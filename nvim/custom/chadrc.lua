51bffc01fc1315847b85820b94938b4f02d77d37
Ian Chesal <ian.chesal@gmail.com>
2023-04-23 18:18:39 -0400

    Try some new colours? Why not!

--- nvim/custom/chadrc.lua
+++ nvim/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require("custom.highlights")
 
 M.ui = {
-	theme = "tokyodark",
-	theme_toggle = { "tokyodark", "tokyodark" },
+	theme = "rxyhn",
+	theme_toggle = { "rxyhn", "rxyhn" },
 
 	hl_override = highlights.override,
 	hl_add = highlights.add,
