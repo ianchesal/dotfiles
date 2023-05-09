64b23abf36d8fb03dda6bca9fdd57f78b3cbc61a
Ian Chesal <ian@withpersona.com>
2023-05-08 21:34:54 -0400

    Hack turning nvim-cmp off/on in a buffer
    Nice for getting it out of the way when you just want to type without
    all the popups in your face.
    
    Still need to figure out how to _toggle_ it off/on for a buffer.

--- nvim/custom/mappings.lua
+++ nvim/custom/mappings.lua
@@ -1,18 +1,29 @@
 ---@type MappingsTable
 local M = {}
 
+-- initialize global var to false -> nvim-cmp turned off per default
+vim.g.cmptoggle = true
+
 M.disabled = {
-	n = {
-		-- ['<leader>b'] = '',
-	},
+  n = {
+    -- ['<leader>b'] = '',
+  },
 }
 
 M.general = {
-	n = {
-		[";"] = { ":", "enter command mode", opts = { nowait = true } },
-		-- ["<leader>bq"] = {"<cmd> Bdelete <CR>", "Delete buffer", opts = { noremap = true } }, -- Use <leader>x in NV Chad
-		["<leader>gg"] = { "<cmd> Git <CR>", "git fugitive", opts = { noremap = true } },
-	},
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    -- ["<leader>bq"] = {"<cmd> Bdelete <CR>", "Delete buffer", opts = { noremap = true } }, -- Use <leader>x in NV Chad
+    ["<leader>gg"] = { "<cmd> Git <CR>", "git fugitive", opts = { noremap = true } },
+    ["<leader>tcf"] = {
+      "<cmd> lua require('cmp').setup.buffer { enabled = false } <CR>",
+      "Turn nvim-cmp off in buffer",
+    },
+    ["<leader>tco"] = {
+      "<cmd> lua require('cmp').setup.buffer { enabled = true } <CR>",
+      "Turn nvim-cmp on in buffer",
+    },
+  },
 }
 
 -- more keybinds!
