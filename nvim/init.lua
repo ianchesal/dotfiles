c229ea9528eab071b693a59cfa3e05042cb7b05e
Ian Chesal <ian@withpersona.com>
2023-05-13 15:34:40 -0400

    Endwise isn't behaving. Forcing it on all the time.

--- nvim/init.lua
+++ nvim/init.lua
@@ -256,6 +256,13 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
+-- Treesitter Endwise Setup
+require('nvim-treesitter.configs').setup {
+  endwise = {
+    enable = true,
+  },
+}
+
 -- LSP settings.
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
