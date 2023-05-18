bbcb8df2b39171f10f6456414c22e66516cc18c7
Ian Chesal <ian@withpersona.com>
2023-05-18 17:07:13 -0400

    Switch to onedark everywhere
    Time for a color change

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,14 +1,14 @@
-  return {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        -- theme = 'onedark',
-        theme = 'tokyonight',
-        component_separators = '|',
-        section_separators = '',
-      },
+return {
+  -- Set lualine as statusline
+  'nvim-lualine/lualine.nvim',
+  -- See `:help lualine.txt`
+  opts = {
+    options = {
+      icons_enabled = false,
+      -- theme = 'onedark', -- auto for theme works pretty well
+      -- theme = 'tokyonight',
+      component_separators = '|',
+      section_separators = '',
     },
-  }
+  },
+}
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,26 +1,27 @@
--- {
---   -- Theme inspired by Atom
---   'navarasu/onedark.nvim',
---   priority = 1000,
---   config = function()
---     vim.cmd.colorscheme 'onedark'
---   end,
--- },
-
 return {
-  'folke/tokyonight.nvim',
+  -- 'folke/tokyonight.nvim',
+  -- config = function()
+  --   require('tokyonight').setup({
+  --     style = 'night',
+  --     transparent = false,
+  --     styles = {
+  --       -- Fuck italics in the terminal. Who wants that shit?
+  --       comments = { italic = false },
+  --       keywords = { italic = false },
+  --       functions = { italic = false },
+  --       variables = { italic = false },
+  --     },
+  --   })
+  --   vim.cmd.colorscheme 'tokyonight'
+  -- end
+  'navarasu/onedark.nvim',
   config = function()
-    require('tokyonight').setup({
-      style = 'night',
-      transparent = false,
-      styles = {
-        -- Fuck italics in the terminal. Who wants that shit?
-        comments = { italic = false },
-        keywords = { italic = false },
-        functions = { italic = false },
-        variables = { italic = false },
+    require('onedark').setup({
+      style = 'darker',      -- Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+      lualine = {
+        transparent = false, -- lualine center bar transparency
       },
     })
-    vim.cmd.colorscheme 'tokyonight'
+    vim.cmd.colorscheme 'onedark'
   end
 }
