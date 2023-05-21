745e1f6d5a4665ff102afa09d823de16aeb12f44
Ian Chesal <ian@withpersona.com>
2023-05-21 10:26:48 -0400

    Oh. Very nice fzf stuff.
    Also trimmed some things from my antidote plugin list here. I don't need
    this much stuff in my shell.

--- zsh/zshrc.d/completion.zsh
+++ zsh/zshrc.d/completion.zsh
@@ -13,3 +13,16 @@ zstyle ':completion:*' matcher-list '' \
 # Don't complete users. It's annoying.
 # See: https://unix.stackexchange.com/questions/57408/why-is-zsh-oh-my-zsh-completing-directories-that-dont-exist
 zstyle ':completion:*' users asgeo1 root ianchesal
+
+# Recommended for use with fzf-tab
+# See: https://github.com/Aloxaf/fzf-tab#configure
+# disable sort when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
+# set descriptions format to enable group support
+zstyle ':completion:*:descriptions' format '[%d]'
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# preview directory's content with exa when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
+# switch group using `,` and `.`
+zstyle ':fzf-tab:*' switch-group ',' '.'
