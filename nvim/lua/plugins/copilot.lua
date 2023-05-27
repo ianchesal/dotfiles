ac1a8801fd53d8a69bc5da470fd81780dac6ab62
Ian Chesal <ian@withpersona.com>
2023-05-27 11:36:40 -0400

    Copilot tweaks

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -3,11 +3,20 @@ return {
   event = "InsertEnter",
   lazy = false,
   opts = {
+    enabled = true,
     suggestion = {
       enable = false,
     },
     panel = {
       enable = false,
+    },
+    filetypes = {
+      yaml = false,
+      markdown = false,
+      help = false,
+      gitcommit = false,
+      gitrebase = false,
+      ["."] = false,
     },
   }
 }
