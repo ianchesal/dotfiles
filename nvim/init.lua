d8df921f956e2d4a0d6b5518a68c5a426402562d
Ian Chesal <ian.chesal@gmail.com>
2023-05-30 12:12:00 +0000

    Try out noice
    It's an interesting idea. Not sure I'll stick with it.

--- nvim/init.lua
+++ nvim/init.lua
@@ -278,7 +278,9 @@ require("nvim-treesitter.configs").setup({
     "lua",
     "make",
     "markdown",
+    "markdown_inline",
     "python",
+    "regex",
     "ruby",
     "rust",
     "terraform",
@@ -603,5 +605,53 @@ cmp.setup({
 
 -- autopairs setup for cmp
 cmp.event:on("confirm.done", cmp_autopairs.on_confirm_done())
+
+-- noice setup
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true,
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true,         -- use a classic bottom cmdline for search
+    command_palette = true,       -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false,       -- add a border to hover docs and signature help
+  },
+})
+
+require("lualine").setup({
+  sections = {
+    lualine_x = {
+      {
+        require("noice").api.status.message.get_hl,
+        cond = require("noice").api.status.message.has,
+      },
+      {
+        require("noice").api.status.command.get,
+        cond = require("noice").api.status.command.has,
+        color = { fg = "#ff9e64" },
+      },
+      {
+        require("noice").api.status.mode.get,
+        cond = require("noice").api.status.mode.has,
+        color = { fg = "#ff9e64" },
+      },
+      {
+        require("noice").api.status.search.get,
+        cond = require("noice").api.status.search.has,
+        color = { fg = "#ff9e64" },
+      },
+    },
+  },
+})
+
+require("telescope").load_extension("noice")
 
 -- vim: ts=2 sts=2 sw=2 et
