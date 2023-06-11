70f296f7382620ad4b9bc05e97a9070ef1809d9f
Ian Chesal <ian.chesal@gmail.com>
2023-06-11 14:10:22 -0400

    Avoid breaking changes in fidget.nvim
    For now pin to the legacy tag

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -17,7 +17,7 @@ return {
 
 		-- Useful status updates for LSP
 		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-		{ 'j-hui/fidget.nvim', opts = {} },
+		{ 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
 
 		-- Additional lua configuration, makes nvim stuff amazing!
 		'folke/neodev.nvim',
