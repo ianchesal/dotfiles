972b07ce59bc4986425da55bcc4decda38d629da
Ian Chesal <ian.chesal@gmail.com>
2023-07-29 14:35:11 +0000

    Get Mason and LSP configurations sorted out in the LazyVim way

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,28 +1,28 @@
-if true then
-  return {}
-end
--- return {
---   "williamboman/mason.nvim",
---   opts = {
---     ensure_installed = {
---       "bash-language-server",
---       "denols",
---       "diagnosticls",
---       "dockerls",
---       "helm_ls",
---       "jsonls",
---       "jsonnet_ls",
---       "lua_ls",
---       "marksman",
---       "rubocop",
---       "sqlls",
---       "solargraph",
---       "shellcheck",
---       "shfmt",
---       "terraformls",
---       "tflint",
---       "tflint",
---       "tsserver",
---     },
---   },
--- }
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "bash-language-server",
+      "deno",
+      "diagnostic-languageserver",
+      "dockerfile-language-server",
+      "hadolint",
+      "helm-ls",
+      "json-lsp",
+      "jsonnet-language-server",
+      "lua-language-server",
+      "marksman",
+      "prettierd",
+      "rubocop",
+      "shellcheck",
+      "shfmt",
+      "solargraph",
+      "sqlls",
+      "stylua",
+      "terraform-ls",
+      "tflint",
+      "typescript-language-server",
+      "yaml-language-server",
+    },
+  },
+}
