ea98a0d7cf3286ea8979c715e20061f66f81cd37
Ian Chesal <ian.chesal@gmail.com>
2023-07-30 16:10:24 +0000

    Prep for playing with colorschemes

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+if true then
+  return {}
+end
+
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
