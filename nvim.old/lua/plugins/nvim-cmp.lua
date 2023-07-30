e05d3160a3a092f52969818bce01ec6a19f78b7f
Ian Chesal <ian.chesal@gmail.com>
2023-07-30 14:41:27 +0000

    Remove my old vim stuff
    LazyVim has far fewer issues with it than my hand-crafted stuff. Fast
    enough.

--- nvim.old/lua/plugins/nvim-cmp.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- Autocompletion
-  'hrsh7th/nvim-cmp',
-  dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip',
-    'rafamadriz/friendly-snippets', "zbirenbaum/copilot-cmp" },
-  config = function()
-    require("copilot_cmp").setup()
-  end,
-}
--- nvim.old/lua/plugins/themes/midnight.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'dasupradyumna/midnight.nvim', -- https://github.com/dasupradyumna/midnight.nvim
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require('midnight').setup()
-  end
-}
