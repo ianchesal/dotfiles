165c10c8cf21586fbcb63679f476e31f093dd458
Ian Chesal <ian.chesal@gmail.com>
2023-08-20 20:37:02 +0000

    Try Hardtime in NeoVim
    Nothing like forced labor to really learn your editor, right?

---
+++ nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,10 @@
+-- Learn better vim habits
+return {
+  "m4xshen/hardtime.nvim",
+  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  opts = {
+    -- Uncomment line below if having arrow keys disabled really drives you nuts
+    -- disabled_keys = {},
+    disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "neo-tree" },
+  },
+}
