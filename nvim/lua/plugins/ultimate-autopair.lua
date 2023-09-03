0d4256be35ae3531512765ee99260213cd45af4f
Ian Chesal <ian.chesal@gmail.com>
2023-09-03 15:07:04 +0000

    Ultimate Autopair looks great, but it's crashing a lot right now
    Try it again later when it's not so crashy.

---
+++ nvim/lua/plugins/ultimate-autopair.lua
@@ -0,0 +1,10 @@
+return {}
+-- return {
+-- "altermo/ultimate-autopair.nvim",
+-- event = { "InsertEnter", "CmdlineEnter" },
+-- dependencies = "nvim-treesitter/nvim-treesitter",
+-- TODO: Go back to v0.6 once nvim v0.9 gets updated. See: https://github.com/altermo/ultimate-autopair.nvim/issues/39
+-- branch = "v0.6", -- TODO: Remove branch when there's a stable main, maybe?
+-- commit = "667d2304e8eb9ddbfa7f962528cfce0a5edcc163",
+-- opts = {},
+-- }
