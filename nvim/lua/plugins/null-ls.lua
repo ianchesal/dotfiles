6fd8e5a4aebb6cd7cbe710bc915f423071cdcb35
Ian Chesal <ian@withpersona.com>
2023-09-07 18:31:08 -0400

    Stage 2 prep for removing my use of null-ls
    Comment all the null-ls integrations I had out and lets see how I do
    without them.

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -8,18 +8,18 @@ return {
       root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
       sources = {
         -- See: https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
-        nls.builtins.formatting.stylua,
-        nls.builtins.formatting.shfmt,
+        -- nls.builtins.formatting.stylua,
+        -- nls.builtins.formatting.shfmt,
         -- webdev stuff
-        nls.builtins.formatting.deno_fmt,
-        nls.builtins.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }),
+        -- nls.builtins.formatting.deno_fmt,
+        -- nls.builtins.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }),
         -- Terraform
-        nls.builtins.formatting.terraform_fmt,
-        nls.builtins.diagnostics.terraform_validate,
+        -- nls.builtins.formatting.terraform_fmt,
+        -- nls.builtins.diagnostics.terraform_validate,
         -- Packer
-        nls.builtins.formatting.packer,
+        -- nls.builtins.formatting.packer,
         -- I dun spel gud
-        nls.builtins.hover.dictionary,
+        -- nls.builtins.hover.dictionary,
       },
     }
   end,
