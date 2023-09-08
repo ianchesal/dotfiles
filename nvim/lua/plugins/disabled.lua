a8ac4c82e4da6e9686b312c2f9bcc0ceb041fcf8
Ian Chesal <ian@withpersona.com>
2023-09-07 21:04:16 -0400

    Stage 3 Actually remove null-ls
    Big moment here. But I think I'll survive.

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
-  { "SmiteshP/nvim-navic", enabled = false },
-  { "kdheepak/lazygit.nvim", enabled = false },
+  { "SmiteshP/nvim-navic",             enabled = false },
+  { "kdheepak/lazygit.nvim",           enabled = false },
   -- { "echasnovski/mini.pairs", enabled = false },
+  { "jose-elias-alvarez/null-ls.nvim", enabled = false },
 }
