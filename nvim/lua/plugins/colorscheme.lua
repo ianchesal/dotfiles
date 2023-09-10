62ddb44ec20c806cddd243888d89464974a82ed3
Ian Chesal <ian.chesal@gmail.com>
2023-09-10 14:47:23 +0000

    Onedark --  with more configuration

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,24 +1,32 @@
 return {
   {
     "AlexvZyl/nordic.nvim",
+    "EdenEast/nightfox.nvim",
     "bluz71/vim-moonfly-colors",
     "bluz71/vim-nightfly-colors",
     "catppuccin/nvim",
     "ellisonleao/gruvbox.nvim",
     "jacoborus/tender.vim",
     "mcchrish/zenbones.nvim",
-    "navarasu/onedark.nvim",
     "projekt0n/github-nvim-theme",
     "rebelot/kanagawa.nvim",
     "rktjmp/lush.nvim",
-    "rmehri01/onenord.nvim",
-    "EdenEast/nightfox.nvim",
+  },
+
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "darker", -- Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+      lualine = {
+        transparent = true,
+      },
+    },
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "moonfly",
+      colorscheme = "onedark",
     },
   },
 }
