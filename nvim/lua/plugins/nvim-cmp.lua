d36e33a08116529865c21b470ac0622daf2de078
Ian Chesal <ian@withpersona.com>
2023-11-14 23:08:41 -0500

    Replace deprecated `nvim_buf_get_option` call

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -4,7 +4,7 @@ return {
     enabled = function()
       local context = require("cmp.config.context")
       local disabled = false
-      disabled = disabled or (vim.api.nvim_buf_get_option(0, "buftype") == "prompt")
+      disabled = disabled or (vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "prompt")
       disabled = disabled or (vim.fn.reg_recording() ~= "")
       disabled = disabled or (vim.fn.reg_executing() ~= "")
       disabled = disabled or context.in_treesitter_capture("comment")
