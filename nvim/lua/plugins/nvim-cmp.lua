17391b42b5a0104af9f1e2785264525f3b704c10
Ian Chesal <ian@withpersona.com>
2023-12-22 13:31:59 -0500

    cmp: require an entry to be selected for <cr> to work
    This was driving me absolutely bonkers. Now cmp requires me to actually
    select something in the completion list for <CR> to work as the
    completion trigger. No longer randomly completing when I hit <CR> will
    save my sanity.

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,3 +1,5 @@
+local cmp = require("cmp")
+
 return {
   "hrsh7th/nvim-cmp",
   config = {
@@ -10,5 +12,18 @@ return {
       disabled = disabled or context.in_treesitter_capture("comment")
       return not disabled
     end,
+    mapping = {
+      ["<CR>"] = cmp.mapping({
+        i = function(fallback)
+          if cmp.visible() and cmp.get_active_entry() then
+            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+          else
+            fallback()
+          end
+        end,
+        s = cmp.mapping.confirm({ select = true }),
+        c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+      }),
+    },
   },
 }
