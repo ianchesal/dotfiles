329fa7831a0e479d8b0e102081f12d9c100da6f7
Ian Chesal <ian.chesal@gmail.com>
2023-12-26 16:20:00 +0000

    cyberdream could be a cool scheme one day
    Needs some refinement though. Will keep an eye on it.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -20,14 +20,29 @@ return {
     "folke/tokyonight.nvim",
     name = "tokyonight",
     opts = {
-      style = "night"
+      style = "night",
     },
+  },
+
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("cyberdream").setup({
+        transparent = true, -- enable transparent background
+        italic_comments = true, -- italicize comments
+        hide_fillchars = true, -- replace all fillchars with ' ' for the ultimate clean look
+      })
+      -- vim.cmd("colorscheme cyberdream") -- set the colorscheme
+    end,
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "tokyonight-night",
+      -- colorscheme = "cyberdream",
     },
   },
 }
