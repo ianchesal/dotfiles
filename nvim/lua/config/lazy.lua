412ff23ca907880457f46a2555e4cea39a8e0c07
Ian Chesal <ian.chesal@gmail.com>
2023-12-31 20:47:20 +0000

    LazyVim got a default setup for markdown-preview
    Don't need to maintain this by hand any more. It's built-in.

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
