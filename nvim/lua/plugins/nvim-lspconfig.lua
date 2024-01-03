2d787677762c4881fa2a15194b4a656417d15e51
Ian Chesal <ian@withpersona.com>
2024-01-03 17:01:27 -0500

    Switch from solargraph/rubocop-lsp to ruby-lsp

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -26,28 +26,35 @@ return {
         -- regols is not maanged by Mason. i install it with `brew install kitagry/tap/regols`.
         -- See: https://github.com/kitagry/regols
         regols = {},
-        rubocop = {
-          -- See: https://docs.rubocop.org/rubocop/usage/lsp.html
-          cmd = { "bundle", "exec", "rubocop", "--lsp" },
-          root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
-        },
-        sqlls = {},
-        solargraph = {
-          -- See: https://medium.com/@cristianvg/neovim-lsp-your-rbenv-gemset-and-solargraph-8896cb3df453
-          cmd = { os.getenv("HOME") .. "/.asdf/shims/solargraph", "stdio" },
-          root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
-          settings = {
-            solargraph = {
-              autoformat = true,
-              completion = true,
-              diagnostics = true,
-              folding = true,
-              references = true,
-              rename = true,
-              symbols = true,
-            },
+        ruby_ls = {
+          cmd = { "bundle", "exec", "ruby-lsp" },
+          init_options = {
+            formatter = "auto",
           },
+          settings = {},
         },
+        -- rubocop = {
+        --   -- See: https://docs.rubocop.org/rubocop/usage/lsp.html
+        --   cmd = { "bundle", "exec", "rubocop", "--lsp" },
+        --   root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
+        -- },
+        sqlls = {},
+        -- solargraph = {
+        --   -- See: https://medium.com/@cristianvg/neovim-lsp-your-rbenv-gemset-and-solargraph-8896cb3df453
+        --   cmd = { os.getenv("HOME") .. "/.asdf/shims/solargraph", "stdio" },
+        --   root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
+        --   settings = {
+        --     solargraph = {
+        --       autoformat = true,
+        --       completion = true,
+        --       diagnostics = true,
+        --       folding = true,
+        --       references = true,
+        --       rename = true,
+        --       symbols = true,
+        --     },
+        --   },
+        -- },
         terraformls = {},
         tsserver = {},
         yamlls = {},
