5322ab1750b6f767a88671b9e15a597f6fa3ef54
Ian Chesal <ian@withpersona.com>
2024-01-04 09:17:32 -0500

    Ensure we use Ruby LSP and not Solargraph
    The "ruby" plugin for LazyVim forces Solargraph on you. This just copies
    that bits out of that plugin that I find useful for Ruby development and
    leaves the rest on the floor. I had already configured Ruby LSP in the
    mason.lua and nvim-lspconfig.lua files.

---
+++ nvim/lua/plugins/ruby-development.lua
@@ -0,0 +1,33 @@
+-- The LazyVim ruby development plugin thingy was defaulting to Solargraph
+-- So this copies what I need from that config but omits the Solargraph stuff
+-- because Ruby LSP is what we're using now.
+-- It copies stuff from here: https://www.lazyvim.org/extras/lang/ruby
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "ruby",
+      })
+    end,
+  },
+  -- {
+  --   "suketa/nvim-dap-ruby",
+  --   config = function()
+  --     require("dap-ruby").setup()
+  --   end,
+  -- },
+  {
+    "olimorris/neotest-rspec",
+  },
+  -- {
+  --   "mfussenegger/nvim-dap",
+  --   optional = true,
+  --   dependencies = {
+  --     "suketa/nvim-dap-ruby",
+  --     config = function()
+  --       require("dap-ruby").setup()
+  --     end,
+  --   },
+  -- },
+}
