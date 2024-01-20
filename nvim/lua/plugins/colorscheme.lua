2ba1d5f6ab40c7c9da45c9abfac66fa4bc16c8a0
Ian Chesal <ian@withpersona.com>
2024-01-20 15:08:53 -0500

    Prune plugins I'm not using

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -16,38 +16,38 @@ return {
   --   name = "catppuccin",
   -- },
 
-  {
-    "folke/tokyonight.nvim",
-    name = "tokyonight",
-    opts = {
-      style = "night",
-    },
-  },
-
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("cyberdream").setup({
-        transparent = false, -- enable transparent background
-        italic_comments = false, -- italicize comments
-        hide_fillchars = true, -- replace all fillchars with ' ' for the ultimate clean look
-        borderless_telescope = false, -- remove borders from telescope windows
-        theme = {
-          colors = {
-            -- Use a slightly darker green
-            green = "#06c258",
-          },
-
-          highlights = {
-            -- Set indent scope to blue (instead of pink)
-            MiniIndentScopeSymbol = { fg = "#5ea1ff" },
-          },
-        },
-      })
-    end,
-  },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   name = "tokyonight",
+  --   opts = {
+  --     style = "night",
+  --   },
+  -- },
+  --
+  -- {
+  --   "scottmckendry/cyberdream.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("cyberdream").setup({
+  --       transparent = false, -- enable transparent background
+  --       italic_comments = false, -- italicize comments
+  --       hide_fillchars = true, -- replace all fillchars with ' ' for the ultimate clean look
+  --       borderless_telescope = false, -- remove borders from telescope windows
+  --       theme = {
+  --         colors = {
+  --           -- Use a slightly darker green
+  --           green = "#06c258",
+  --         },
+  --
+  --         highlights = {
+  --           -- Set indent scope to blue (instead of pink)
+  --           MiniIndentScopeSymbol = { fg = "#5ea1ff" },
+  --         },
+  --       },
+  --     })
+  --   end,
+  -- },
 
   {
     "navarasu/onedark.nvim",
