d9f3dc65d956d37b4d3589c5f923bf8127971bce
Ian Chesal <ian.chesal@gmail.com>
2024-02-03 22:50:13 +0000

    I like to play with crayons

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -19,38 +19,38 @@ local cyberdream_dashboard = {
 }
 
 return {
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   name = "tokyonight",
-  --   opts = {
-  --     style = "night",
-  --   },
-  -- },
-  --
-  -- {
-  --   "scottmckendry/cyberdream.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("cyberdream").setup({
-  --       transparent = false, -- enable transparent background
-  --       italic_comments = false, -- italicize comments
-  --       hide_fillchars = true, -- replace all fillchars with ' ' for the ultimate clean look
-  --       borderless_telescope = false, -- remove borders from telescope windows
-  --       theme = {
-  --         colors = {
-  --           -- Use a slightly darker green
-  --           green = "#06c258",
-  --         },
-  --
-  --         highlights = {
-  --           -- Set indent scope to blue (instead of pink)
-  --           MiniIndentScopeSymbol = { fg = "#5ea1ff" },
-  --         },
-  --       },
-  --     })
-  --   end,
-  -- },
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
+    opts = {
+      style = "night",
+    },
+  },
+
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("cyberdream").setup({
+        transparent = false, -- enable transparent background
+        italic_comments = false, -- italicize comments
+        hide_fillchars = true, -- replace all fillchars with ' ' for the ultimate clean look
+        borderless_telescope = false, -- remove borders from telescope windows
+        theme = {
+          colors = {
+            -- Use a slightly darker green
+            green = "#06c258",
+          },
+
+          highlights = {
+            -- Set indent scope to blue (instead of pink)
+            MiniIndentScopeSymbol = { fg = "#5ea1ff" },
+          },
+        },
+      })
+    end,
+  },
 
   {
     "navarasu/onedark.nvim",
@@ -65,12 +65,20 @@ return {
     },
   },
 
+  -- {
+  --   "sontungexpt/witch",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  -- },
+
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "tokyonight-night",
       colorscheme = "onedark",
       -- colorscheme = "cyberdream",
+      -- colorscheme = "witch",
     },
   },
 }
