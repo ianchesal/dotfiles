762126c3e7cea8fa25c8981fcbdb56fd3cca294c
Ian Chesal <ian@withpersona.com>
2024-02-06 23:04:13 -0500

    I apparently don't need this
    This was never a plugin LazyVim was using. So now I have no idea why I
    thought I needed it.

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
-  { "SmiteshP/nvim-navic",    enabled = false },
-  { "kdheepak/lazygit.nvim",  enabled = false },
+  { "SmiteshP/nvim-navic", enabled = false },
+  -- { "kdheepak/lazygit.nvim",  enabled = false },
   -- { "echasnovski/mini.pairs", enabled = false },
   { "nvimtools/none-ls.nvim", enabled = false },
 }
