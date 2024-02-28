03b8fe3e3141a21b0c73ea18e9d6b03943e463b5
Ian Chesal <ian@withpersona.com>
2024-02-28 08:36:18 -0500

    Make snappier

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,7 +3,7 @@
 -- Add any additional options here
 
 -- Some OS detectors
-local is_wsl = vim.fn.has('wsl') == 1
+local is_wsl = vim.fn.has("wsl") == 1
 -- local is_mac = vim.fn.has("macunix") == 1
 -- local is_linux = not is_wsl and not is_mac
 
@@ -14,20 +14,22 @@ opt.confirm = false
 opt.breakindent = true
 -- opt.cindent = true
 opt.visualbell = true
+-- Make snappier
+opt.timeoutlen = 50
 
 -- WSL Clipboard support
 if is_wsl then
   -- This is NeoVim's recommended way to solve clipboard sharing if you use WSL
   -- See: https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
   vim.g.clipboard = {
-    name = 'WslClipboard',
+    name = "WslClipboard",
     copy = {
-      ['+'] = 'clip.exe',
-      ['*'] = 'clip.exe',
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
     },
     paste = {
-      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
     },
     cache_enabled = 0,
   }
