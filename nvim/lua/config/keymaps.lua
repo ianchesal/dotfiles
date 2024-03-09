276725b12751581108da00922afe6035446dc54e
Ian Chesal <ian.chesal@gmail.com>
2024-03-09 14:40:20 +0000

    Modify keymaps with which-key
    This is the more correct way to do all of this stuff in a neovim world
    where I'm running which-key.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,13 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
--- Unmap mappings used by tmux plugin
---
--- TODO(ian): There's likely a better way to do this.
--- Only switch to TMuxNavgivate if we're in a tmux session
 
-local Util = require("lazyvim.util")
+local util = require("lazyvim.util")
 local map = vim.keymap
+local my_keymaps = {}
 
 -- Only set this up if we're in a tmux session
 if os.getenv("TMUX") then
@@ -20,12 +16,34 @@ if os.getenv("TMUX") then
   map.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>")
   map.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>")
 
-  map.set("n", "<leader>vp", ":VimuxPromptCommand<cr>", { desc = "Run command in tmux shell" })
-  map.set("n", "<leader>vl", ":VimuxRunLastCommand<cr>", { desc = "Run last shell command" })
-  map.set("n", "<leader>vi", ":VimuxInspectRunner<cr>", { desc = "Inspect tmux shell" })
-  map.set("n", "<leader>vz", ":VimuxZoomRunner<cr>", { desc = "Zoom tmux shell" })
-  map.set("n", "<leader>vk", ":VimuxCloseRunner<cr>", { desc = "Close tmux shell" })
-  map.set("n", "<leader>vb", ":VimuxInterruptRunner<cr>", { desc = "Interrupt command running in tmux shell" })
+  my_keymaps["v"] = {
+    name = "vimux",
+    p = {
+      "<cmd>VimuxPromptCommand<cr>",
+      "Run command in tmux shell",
+    },
+    l = {
+      "<cmd>VimuxRunLastCommand<cr>",
+      "Run last shell command",
+    },
+    i = {
+      "<cmd>VimuxInspectRunner<cr>",
+      "Inspect tmux shell",
+    },
+    z = {
+      "<cmd>VimuxZoomRunner<cr>",
+      "Zoom tmux shell",
+    },
+    k = {
+      "<cmd>VimuxCloseRunner<cr>",
+      "Close tmux shell",
+    },
+    b = {
+      "<cmd>VimuxInterruptRunner<cr>",
+      "Interrupt command running in tmux shell",
+    },
+  }
+
   vim.g.VimuxHeight = "30"
   vim.g.VimuxCloseOnExit = 1
 end
@@ -33,21 +51,60 @@ end
 -- Git fugitive for me
 map.del("n", "<leader>gg")
 map.del("n", "<leader>gG")
-map.set("n", "<leader>gg", ":Git<cr>", { desc = "fugitive" })
-map.set("n", "<leader>gl", ":Git log<cr>", { desc = "view logs" })
-map.set("n", "<leader>ga", ":Git add %:p<cr>", { desc = "add current file" })
-map.set("n", "<leader>gp", ":Git push origin HEAD<cr>", { desc = "push orgin HEAD" })
+my_keymaps["g"] = {
+  name = "git",
+  g = {
+    "<cmd>Git<cr>",
+    "Fugitive",
+  },
+  l = {
+    "<cmd>Git log<cr>",
+    "View logs",
+  },
+  a = {
+    "<cmd>Git add %:p<cr>",
+    "Add current file",
+  },
+  p = {
+    "<cmd>Git push origin HEAD<cr>",
+    "Push: origin --> HEAD",
+  },
+}
 
--- I prefer these keymaps for Lazy and Mason and LSP interactions
+-- I prefer different keymaps for Lazy and Mason and LSP interactions
 map.del("n", "<leader>l")
--- map.del("n", "<leader>cl")
-map.set("n", "<leader>lg", ":LspLog<cr>", { desc = "Open LSP logs" })
-map.set("n", "<leader>lh", ":LazyHealth<cr>", { desc = "Health diagnostics" })
-map.set("n", "<leader>li", ":LspInfo<cr>", { desc = "Open LspInfo interface" })
-map.set("n", "<leader>ll", ":Lazy<cr>", { desc = "Open Lazy management interface" })
-map.set("n", "<leader>lm", ":Mason<cr>", { desc = "Open Mason management interface" })
-map.set("n", "<leader>ln", "<cmd>NullLsInfo<CR>", { desc = "NullLS Information" })
-map.set("n", "<leader>lr", ":LspRestart<cr>", { desc = "Restart LSP" })
+my_keymaps["l"] = {
+  -- Mason and LSP
+  name = "lsp",
+  g = {
+    "<cmd>LspLog<cr>",
+    "Open LSP logs",
+  },
+  h = {
+    "<cmd>LazyHealth<cr>",
+    "Health diagnostics",
+  },
+  i = {
+    "<cmd>LspInfo<cr>",
+    "Open LspInfo interface",
+  },
+  l = {
+    "<cmd>Lazy<cr>",
+    "Open Lazy management interface",
+  },
+  m = {
+    "<cmd>Mason<cr>",
+    "Open Mason management interface",
+  },
+  -- n = {
+  --   "<cmd>NullLsInfo<cr>",
+  --   "NullLS Information",
+  -- },
+  r = {
+    "<cmd>LspRestart<cr>",
+    "Restart all LSPs",
+  },
+}
 
 -- Copilot
 local copilot_on = false -- I start with Copilot OFF
@@ -63,20 +120,41 @@ vim.api.nvim_create_user_command("CopilotToggle", function()
 end, { nargs = 0 })
 map.set("", "<leader>ug", ":CopilotToggle<CR>", { noremap = true, silent = true })
 
--- MarkdownPreview
-map.set("n", "<leader>mp", ":MarkdownPreviewToggle<cr>", { desc = "Toggle Markdown preview" })
+-- Telescope
+my_keymaps["t"] = {
+  name = "telescope",
+  k = {
+    "<cmd>Telescope keymaps<cr>",
+    "Seach keymaps",
+  },
+  t = {
+    name = "terraform",
+    t = {
+      "<cmd>Telescope terraform_doc<cr>",
+      "Search Terraform documents",
+    },
+    m = {
+      "<cmd>Telescope terraform_doc modules<cr>",
+      "Search Terraform modules",
+    },
+    g = {
+      "<cmd>Telescope terraform_doc full_name=hashicorp/google<cr>",
+      "Search Terraform: google provider",
+    },
+  },
+}
+
+-- Make U the redo command
+-- map.set("n", "U", "<C-r>")
 
 -- Tab to move to between buffers
-if Util.has("bufferline.nvim") then
+if util.has("bufferline.nvim") then
   map.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
   map.set("n", "<Tab>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
 else
   map.set("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
   map.set("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 end
-
--- Make U the redo command
-map.set("n", "U", "<C-r>")
 
 -- I am too old to re-learn how to yank and paste a whole line in vim
 vim.cmd([[noremap Y Y]])
@@ -88,3 +166,5 @@ cnoreabbrev <expr> Q ((getcmdtype() is# ':' && getcmdline() is# 'Q')?('q'):('Q')
 cnoreabbrev <expr> WQ ((getcmdtype() is# ':' && getcmdline() is# 'WQ')?('wq'):('WQ'))
 cnoreabbrev <expr> Wq ((getcmdtype() is# ':' && getcmdline() is# 'Wq')?('wq'):('Wq'))
 ]])
+
+require("which-key").register(my_keymaps, { prefix = "<leader>" })
