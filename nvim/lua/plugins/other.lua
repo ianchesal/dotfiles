bd5ee0b844e37ead2b719ca0f367c3c7dd79055d
Ian Chesal <ian@withpersona.com>
2024-03-09 15:23:22 -0500

    Fix other's configuration
    It didn't like being lazy loaded or configured in the lazy way.

--- nvim/lua/plugins/other.lua
+++ nvim/lua/plugins/other.lua
@@ -1,14 +1,16 @@
 return {
   -- https://github.com/rgroli/other.nvim
   "rgroli/other.nvim",
-  lazy = true,
-  opts = {
-    mappings = {
-      "livewire",
-      "angular",
-      "laravel",
-      "rails",
-      "golang",
-    },
-  },
+  lazy = false,
+  config = function()
+    require("other-nvim").setup({
+      mappings = {
+        "livewire",
+        "angular",
+        "laravel",
+        "rails",
+        "golang",
+      },
+    })
+  end,
 }
