2a7919cd5683539992b11c6283621c2a1ecda85f
Ian Chesal <ian.chesal@gmail.com>
2024-04-16 12:19:47 +0000

    It couldn't be that easy could it?
    This is my attempt to get rid of the deprecated ruby_ls and replace it
    with the new hotness ruby_lsp.
    
    Also: bring back the rubocop lsp.

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -26,35 +26,18 @@ return {
         -- regols is not maanged by Mason. i install it with `brew install kitagry/tap/regols`.
         -- See: https://github.com/kitagry/regols
         regols = {},
-        ruby_ls = {
+        ruby_lsp = {
           cmd = { "bundle", "exec", "ruby-lsp" },
           init_options = {
             formatter = "auto",
           },
-          settings = {},
         },
-        -- rubocop = {
-        --   -- See: https://docs.rubocop.org/rubocop/usage/lsp.html
-        --   cmd = { "bundle", "exec", "rubocop", "--lsp" },
-        --   root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
-        -- },
+        rubocop = {
+          -- See: https://docs.rubocop.org/rubocop/usage/lsp.html
+          cmd = { "bundle", "exec", "rubocop", "--lsp" },
+          root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
+        },
         sqlls = {},
-        -- solargraph = {
-        --   -- See: https://medium.com/@cristianvg/neovim-lsp-your-rbenv-gemset-and-solargraph-8896cb3df453
-        --   cmd = { os.getenv("HOME") .. "/.asdf/shims/solargraph", "stdio" },
-        --   root_dir = lspconfig.util.root_pattern("Gemfile", ".git", "."),
-        --   settings = {
-        --     solargraph = {
-        --       autoformat = true,
-        --       completion = true,
-        --       diagnostics = true,
-        --       folding = true,
-        --       references = true,
-        --       rename = true,
-        --       symbols = true,
-        --     },
-        --   },
-        -- },
         terraformls = {},
         tsserver = {},
         yamlls = {},
