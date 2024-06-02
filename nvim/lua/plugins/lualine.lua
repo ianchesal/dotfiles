76ff497a8eca47cc91fb9c38b3d269a343696b0f
Ian Chesal <ian.chesal@gmail.com>
2024-06-02 17:20:50 +0000

    Edgy is awesome! Use it!
    Edgy is really cool. Not sure why I didn't think to try it sooner.
    
    Drop the example.lua file -- had lots of errors in it now. It's very out
    of date.
    
    Fix some deprecation warnings in other places.

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
       opts.sections.lualine_z = {
         {
           function()
-            local lsps = vim.lsp.get_active_clients({ bufnr = vim.fn.bufnr() })
+            local lsps = vim.lsp.get_clients({ bufnr = vim.fn.bufnr() })
             -- local icon = require("nvim-web-devicons").get_icon_by_filetype(vim.api.nvim_buf_get_option(0, "filetype"))
             if lsps and #lsps > 0 then
               local names = {}
