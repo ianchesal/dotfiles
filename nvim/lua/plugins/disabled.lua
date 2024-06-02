97e074a8fe05286c19bd9c5cd3c85b9d4d1c0d55
Ian Chesal <ian.chesal@gmail.com>
2024-06-02 17:20:50 +0000

    Edgy is awesome! Use it!
    Edgy is really cool. Not sure why I didn't think to try it sooner.
    
    Drop the example.lua file -- had lots of errors in it now. It's very out
    of date.
    
    Fix some deprecation warnings in other places.

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   -- { "kdheepak/lazygit.nvim",  enabled = false },
   -- { "echasnovski/mini.pairs", enabled = false },
   { "nvimtools/none-ls.nvim", enabled = false },
+  -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
