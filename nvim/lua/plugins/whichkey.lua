3b040e3b8ead9e446c9e29f4f2bb2c4c7a613d44
Ian Chesal <ian.chesal@gmail.com>
2024-07-13 02:05:05 +0000

    New which-key configuration

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -2,11 +2,10 @@ return {
   {
     "folke/which-key.nvim",
     opts = {
-      plugins = { spelling = false },
-      defaults = {
-        ["<leader>l"] = { name = "+lsp" },
-        ["<leader>m"] = { name = "+markdown" },
-        ["<leader>v"] = { name = "+terminal" },
+      plugins = {
+        spelling = {
+          enabled = false,
+        },
       },
     },
   },
