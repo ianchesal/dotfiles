2cfb104005bc34e5e491deb6f865991c2e915360
Ian Chesal <ian.chesal@gmail.com>
2024-08-09 21:19:09 -0400

    Proper vi-mode!

--- zsh/zshrc.d/zz-vim-mode.zsh
+++ zsh/zshrc.d/zz-vim-mode.zsh
@@ -1,10 +1,4 @@
-# Enable Ctrl-x-e to edit command line
+bindkey -v
 autoload -U edit-command-line
-# Emacs style
 zle -N edit-command-line
-bindkey '^xe' edit-command-line
-bindkey '^x^e' edit-command-line
-echo "HERE!"
-# Vi style:
-# zle -N edit-command-line
-# bindkey -M vicmd v edit-command-line
+bindkey -M vicmd v edit-command-line
