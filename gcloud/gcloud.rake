187b15b31a8017d37df5489cd5e1e59f18e268bd
Ian Chesal <ian@withpersona.com>
2024-08-09 22:16:48 -0400

    Make gcloud work again
    Such a PIA but this is what we have to resort to...

--- gcloud/gcloud.rake
+++ gcloud/gcloud.rake
@@ -3,13 +3,9 @@ desc 'Keep Google Cloud CLI stuff up-to-date'
 namespace :gcloud do
   desc 'Update gcloud commmand line components'
   task :update do
-    if File.file?('/opt/homebrew/bin/gcloud') || File.file?('/usr/local/bin/gcloud')
-      puts 'Update: gcloud components'.green
-      sh 'gcloud components update --quiet'
-      sh '$(gcloud info --format="value(basic.python_location)") -m pip install numpy'
-    else
-      puts 'No updates to gcloud components performance -- no gcloud CLI found'.red
-    end
+    puts 'Update: gcloud components'.green
+    sh 'type gcloud >/dev/null && gcloud components update --quiet'
+    # sh 'type gcloud >/dev/null && $(gcloud info --format="value(basic.python_location)") -m pip install numpy'
   end
 end
 
--- zsh/zshrc.d/gcloud.zsh
+++ zsh/zshrc.d/gcloud.zsh
@@ -10,28 +10,10 @@
 # fi
 # See: https://cloud.google.com/sdk/crypto
 # Required to work with legacy p12 keyfiles
-export CLOUDSDK_PYTHON_SITEPACKAGES=1
+# export CLOUDSDK_PYTHON_SITEPACKAGES=1
 # See: https://cloud.google.com/blog/products/containers-kubernetes/kubectl-auth-changes-in-gke
 export USE_GKE_GCLOUD_AUTH_PLUGIN=True
-
-if [[ -z "${CLOUDSDK_HOME}" ]]; then
-  search_locations=(
-    "$HOME/google-cloud-sdk"
-    "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
-    "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
-    "/usr/share/google-cloud-sdk"
-    "/snap/google-cloud-sdk/current"
-    "/usr/lib64/google-cloud-sdk/"
-    "/opt/google-cloud-sdk"
-  )
-
-  for gcloud_sdk_location in $search_locations; do
-    if [[ -d "${gcloud_sdk_location}" ]]; then
-      CLOUDSDK_HOME="${gcloud_sdk_location}"
-      break
-    fi
-  done
-fi
+export CLOUDSDK_HOME="$HOME/src/google-cloud-sdk"
 
 if (( ${+CLOUDSDK_HOME} )); then
   if (( ! $+commands[gcloud] )); then
