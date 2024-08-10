9b88a6826b58c6d7256c42b3a9d5cd209547a9dc
Ian Chesal <ian.chesal@gmail.com>
2024-08-09 21:16:04 -0400

    Get up-arrow working for history, emacs mode for command line editing
    Order matters.

---
+++ zsh/zshrc.d/zz-vim-mode.zsh
@@ -0,0 +1,10 @@
+# Enable Ctrl-x-e to edit command line
+autoload -U edit-command-line
+# Emacs style
+zle -N edit-command-line
+bindkey '^xe' edit-command-line
+bindkey '^x^e' edit-command-line
+echo "HERE!"
+# Vi style:
+# zle -N edit-command-line
+# bindkey -M vicmd v edit-command-line
