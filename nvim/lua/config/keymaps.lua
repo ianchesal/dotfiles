6e6d68dcf1512ad50e30674562a562cdf35e7a63
Ian Chesal <ian@withpersona.com>
2024-09-26 09:46:39 -0400

    Remove Copilot configuration
    I'm not using Copilot at all anymore.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -27,18 +27,18 @@ map.del("n", "<leader>gG")
 map.del("n", "<leader>l")
 
 -- Copilot
-local copilot_on = false -- I start with Copilot OFF
-vim.api.nvim_create_user_command("CopilotToggle", function()
-  if copilot_on then
-    vim.cmd("Copilot disable")
-    print("Copilot OFF")
-  else
-    vim.cmd("Copilot enable")
-    print("Copilot ON")
-  end
-  copilot_on = not copilot_on
-end, { nargs = 0 })
-map.set("", "<leader>ug", ":CopilotToggle<CR>", { noremap = true, silent = true })
+-- local copilot_on = false -- I start with Copilot OFF
+-- vim.api.nvim_create_user_command("CopilotToggle", function()
+--   if copilot_on then
+--     vim.cmd("Copilot disable")
+--     print("Copilot OFF")
+--   else
+--     vim.cmd("Copilot enable")
+--     print("Copilot ON")
+--   end
+--   copilot_on = not copilot_on
+-- end, { nargs = 0 })
+-- map.set("", "<leader>ug", ":CopilotToggle<CR>", { noremap = true, silent = true })
 
 -- Make U the redo command
 -- map.set("n", "U", "<C-r>")
