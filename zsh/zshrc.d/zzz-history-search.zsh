4f3072d41b6d7e86b6a2f79f830923ab3ca8d56f
Ian Chesal <ian@withpersona.com>
2024-10-31 11:38:32 -0400

    History search sub-string
    With up/down arrows bound to traverse the history like I like.

--- zsh/zshrc.d/zzz-history-search.zsh
+++ zsh/zshrc.d/zzz-history-search.zsh
@@ -1,12 +1,4 @@
-# start typing + [Up-Arrow] - fuzzy find history forward
-if [[ "${terminfo[kcuu1]}" != "" ]]; then
-  autoload -U up-line-or-beginning-search
-  zle -N up-line-or-beginning-search
-  bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
-fi
-# start typing + [Down-Arrow] - fuzzy find history backward
-if [[ "${terminfo[kcud1]}" != "" ]]; then
-  autoload -U down-line-or-beginning-search
-  zle -N down-line-or-beginning-search
-  bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
-fi
+# zsh-history-substring-search configuration
+bindkey '^[[A' history-substring-search-up   # or '\eOA'
+bindkey '^[[B' history-substring-search-down # or '\eOB'
+HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
