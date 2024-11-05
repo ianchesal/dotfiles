2631ebef00478a066c74e2d753df74bfc6b5dc99
Ian Chesal <ian@withpersona.com>
2024-11-05 11:34:39 -0500

    Fix up history stuff
    This kind of works. Sorta.

--- zsh/zshrc.d/zzz-history-search.zsh
+++ zsh/zshrc.d/zzz-history-search.zsh
@@ -1,4 +1,4 @@
 # zsh-history-substring-search configuration
-bindkey '^[[A' history-substring-search-up   # or '\eOA'
-bindkey '^[[B' history-substring-search-down # or '\eOB'
+# bindkey '^[[A' history-substring-search-up   # or '\eOA'
+# bindkey '^[[B' history-substring-search-down # or '\eOB'
 HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
