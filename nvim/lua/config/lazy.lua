5f3bdee5c3dd478960096f39c12b5599c9a881d3
Ian Chesal <ian.chesal@gmail.com>
2024-11-09 11:52:30 -0500

    Disable these

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -40,9 +40,9 @@ require("lazy").setup({
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
