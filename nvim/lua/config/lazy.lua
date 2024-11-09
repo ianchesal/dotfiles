0550bfd8956521faa1615f4ec301933d2924068d
Ian Chesal <ian.chesal@gmail.com>
2024-11-09 15:38:22 -0500

    Revert to older LazyVim
    New LazyVim versions are no bueno right now. Roll back.

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -32,7 +32,7 @@ require("lazy").setup({
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
-    enabled = true, -- check for plugin updates periodically
+    enabled = false, -- check for plugin updates periodically
     notify = false, -- notify on update
   }, -- automatically check for plugin updates
   performance = {
