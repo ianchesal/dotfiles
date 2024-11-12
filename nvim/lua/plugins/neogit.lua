05233c02187e15eb976368ee65d7320db126e8bd
Ian Chesal <ian@withpersona.com>
2024-11-11 20:01:49 -0500

    Try out Neogit
    Fugitive-esque, but Lua-based with a bunch of sane defaults for
    keybindings and popup windows? Sure...why not?

---
+++ nvim/lua/plugins/neogit.lua
@@ -0,0 +1,17 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- required
+    "sindrets/diffview.nvim", -- optional - Diff integration
+
+    -- Only one of these is needed.
+    "nvim-telescope/telescope.nvim", -- optional
+    "ibhagwan/fzf-lua", -- optional
+    "echasnovski/mini.pick", -- optional
+  },
+  config = function()
+    require("neogit").setup({
+      kind = "floating",
+    })
+  end,
+}
