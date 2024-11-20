a5c96a4c871fe0d0e49fddbc64f7773f64032cda
Ian Chesal <ian.chesal@gmail.com>
2024-11-19 21:39:40 -0500

    New dashboard
    Just playing around with tweaking the dashboard using folke's awesome
    new addition to snacks!

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        sections = {
+          {
+            section = "header",
+            padding = 2,
+          },
+          {
+            section = "keys",
+            padding = 2,
+          },
+          {
+            section = "terminal",
+            icon = " ",
+            title = "Git Status",
+            enabled = vim.fn.isdirectory(".git") == 1,
+            cmd = "git diff --stat -B -M -C",
+            height = 5,
+            indent = 0,
+          },
+          -- {
+          --   section = "startup",
+          -- },
+        },
+      },
+    },
+  },
+}
