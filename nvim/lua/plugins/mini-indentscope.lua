bf45d88eefb88e4477d7cee5fada38c358701464
Ian Chesal <ian@withpersona.com>
2024-11-20 12:45:53 -0500

    Disable customizations on mini.indentscope
    This is causing mini.indentscope to fire on the dashboard which is not
    what I want. Use the default config for now.

--- nvim/lua/plugins/mini-indentscope.lua
+++ nvim/lua/plugins/mini-indentscope.lua
@@ -1,8 +1,8 @@
 return {
-  "echasnovski/mini.indentscope",
-  opts = {
-    draw = {
-      animation = require("mini.indentscope").gen_animation.none(),
-    },
-  },
+  -- "echasnovski/mini.indentscope",
+  -- opts = {
+  --   draw = {
+  --     animation = require("mini.indentscope").gen_animation.none(),
+  --   },
+  -- },
 }
