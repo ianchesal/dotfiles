1b2fe5b152629d530031073f2081d96be07e2e46
Ian Chesal <ian.chesal@gmail.com>
2024-12-14 15:44:38 -0500

    This was throwing errors
    In a brand new buffer that had no type attached to it, this was causing
    issues.

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -5,13 +5,11 @@ return {
       preset = "default",
     },
     enabled = function()
-      local node = vim.treesitter.get_node()
       local disabled = false
       disabled = disabled or (vim.tbl_contains({ "markdown" }, vim.bo.filetype))
       disabled = disabled or (vim.bo.buftype == "prompt")
       disabled = disabled or (vim.fn.reg_recording() ~= "")
       disabled = disabled or (vim.fn.reg_executing() ~= "")
-      -- disabled = disabled or (node and string.find(node:type(), "comment"))
       return not disabled
     end,
   },
