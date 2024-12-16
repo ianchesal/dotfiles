cfa6f12428c64e81ac1882d5b6df5345d9aceeeb
Ian Chesal <ian@withpersona.com>
2024-12-16 08:00:27 -0500

    Deps updates

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,16 +1,16 @@
-return {
-  "saghen/blink.cmp",
-  opts = {
-    keymap = {
-      preset = "super-tab",
-    },
-    enabled = function()
-      local disabled = false
-      disabled = disabled or (vim.tbl_contains({ "markdown" }, vim.bo.filetype))
-      disabled = disabled or (vim.bo.buftype == "prompt")
-      disabled = disabled or (vim.fn.reg_recording() ~= "")
-      disabled = disabled or (vim.fn.reg_executing() ~= "")
-      return not disabled
-    end,
-  },
-}
+return {}
+--   "saghen/blink.cmp",
+--   opts = {
+--     keymap = {
+--       preset = "super-tab",
+--     },
+--     enabled = function()
+--       local disabled = false
+--       disabled = disabled or (vim.tbl_contains({ "markdown" }, vim.bo.filetype))
+--       disabled = disabled or (vim.bo.buftype == "prompt")
+--       disabled = disabled or (vim.fn.reg_recording() ~= "")
+--       disabled = disabled or (vim.fn.reg_executing() ~= "")
+--       return not disabled
+--     end,
+--   },
+-- }
