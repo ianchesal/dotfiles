59d6617ef1828417dc7c645ae42fb8248c9b316d
Ian Chesal <ian.chesal@gmail.com>
2024-12-24 09:15:35 -0500

    Simplify blink.cmp config
    The latest updates to this and Lazy make this stuff unneccesary. It's
    behaving correctly without this extra config now.
    
    I still can't figure out how to turn it off in comments though...

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,20 +1,14 @@
 return {
   "saghen/blink.cmp",
   opts = {
-    keymap = {
-      preset = "super-tab",
-    },
-    completion = {
-      list = {
-        selection = "manual",
-      },
-    },
     enabled = function()
       local disabled = false
+      -- local node = vim.treesitter.get_node()
       disabled = disabled or (vim.tbl_contains({ "markdown" }, vim.bo.filetype))
       disabled = disabled or (vim.bo.buftype == "prompt")
       disabled = disabled or (vim.fn.reg_recording() ~= "")
       disabled = disabled or (vim.fn.reg_executing() ~= "")
+      -- disabled = disabled or (node ~= nil and string.find(node:type(), "comment") ~= nil)
       return not disabled
     end,
   },
