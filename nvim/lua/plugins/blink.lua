88baef458035ea01d3242dc2094fe5b79e12f60f
Ian Chesal <ian.chesal@gmail.com>
2025-01-04 10:12:57 -0500

    Attempting to get blink.cmp to stop in a comment
    Still haven't been able to revive this part of what I liked in the other
    snippets plugin. I don't want blink.cmp to do completions in comment
    blocks when I'm writing code.
    
    Following the blink.cmp docs this seems like it should work, but it does
    not. :(

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -3,13 +3,24 @@ return {
   opts = {
     enabled = function()
       local disabled = false
-      -- local node = vim.treesitter.get_node()
+      -- local success, node = pcall(vim.treesitter.get_node)
       disabled = disabled or (vim.tbl_contains({ "markdown" }, vim.bo.filetype))
       disabled = disabled or (vim.bo.buftype == "prompt")
       disabled = disabled or (vim.fn.reg_recording() ~= "")
       disabled = disabled or (vim.fn.reg_executing() ~= "")
-      -- disabled = disabled or (node ~= nil and string.find(node:type(), "comment") ~= nil)
+      -- disabled = disabled
+      --   or (success and node and vim.tbl_contains({ "comment", "line_comment", "block_comment" }, node:type()))
       return not disabled
     end,
+    sources = {
+      default = function()
+        local success, node = pcall(vim.treesitter.get_node)
+        if success and node and vim.tbl_contains({ "comment", "line_comment", "block_comment" }, node:type()) then
+          return { "buffer" }
+        else
+          return { "lsp", "path", "snippets", "buffer", "copilot" }
+        end
+      end,
+    },
   },
 }
