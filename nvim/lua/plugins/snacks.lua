b055519e972cbdc1cd25a225fb95ccb5661011f3
Ian Chesal <ian.chesal@gmail.com>
2025-01-11 15:25:38 -0500

    Tee hee hee
    Got my custom dashboard header. Finally.

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -7,28 +7,16 @@ return {
         enabled = false,
       },
       dashboard = {
-        sections = {
-          {
-            section = "header",
-            padding = 2,
-          },
-          {
-            section = "keys",
-            padding = 2,
-          },
-          {
-            section = "terminal",
-            icon = " ",
-            title = "Git Status",
-            enabled = vim.fn.isdirectory(".git") == 1,
-            cmd = "git diff --stat -B -M -C",
-            height = 5,
-            indent = 2,
-            ttl = 5,
-          },
-          {
-            section = "startup",
-          },
+        preset = {
+          -- See: https://patorjk.com/software/taag/#p=display&v=0&f=ANSI%20Shadow&t=IRC
+          header = [[
+██╗██████╗  ██████╗
+██║██╔══██╗██╔════╝
+██║██████╔╝██║     
+██║██╔══██╗██║     
+██║██║  ██║╚██████╗
+╚═╝╚═╝  ╚═╝ ╚═════╝
+        ]],
         },
       },
     },
