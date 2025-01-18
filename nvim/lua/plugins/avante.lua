89dd6bb5baa75ef4c92f811a8f1a3047ab282539
Ian Chesal <ian.chesal@gmail.com>
2025-01-18 15:48:24 -0500

    Get Avante set plugins
    This is working and without throwing errors but it's using Copilot when
    I want to use Anthropic's claude-3 with my API key.
    
    I stole this from https://github.com/tku137/lazyvim/blob/main/lua/plugins/ai.lua

---
+++ nvim/lua/plugins/avante.lua
@@ -0,0 +1,82 @@
+local prefix = "<Leader>a"
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = true,
+    version = false, -- set this if you want to always pull the latest change
+    enabled = function(_, _)
+      local api_key = os.getenv("ANTHROPIC_API_KEY")
+      return api_key ~= nil and api_key ~= ""
+    end,
+    opts = {
+      -- add any opts here
+      mappings = {
+        ask = prefix .. "<CR>",
+        edit = prefix .. "e",
+        refresh = prefix .. "r",
+        focus = prefix .. "f",
+        toggle = {
+          default = prefix .. "t",
+          debug = prefix .. "d",
+          hint = prefix .. "h",
+          suggestion = prefix .. "s",
+          repomap = prefix .. "R",
+        },
+        diff = {
+          next = "]c",
+          prev = "[c",
+        },
+        files = {
+          add_current = prefix .. ".",
+        },
+      },
+      behaviour = {
+        auto_suggestions = false,
+      },
+      provider = "copilot",
+      copilot = {
+        model = "claude-3.5-sonnet",
+        temperature = 0,
+        max_tokens = 8192,
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    -- dynamically build it, taken from astronvim
+    build = "make",
+    dependencies = {
+      -- "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        dependencies = {
+          -- make sure rendering happens even without opening a markdown file first
+          "yetone/avante.nvim",
+        },
+        opts = function(_, opts)
+          opts.file_types = opts.file_types or { "markdown", "norg", "rmd", "org" }
+          vim.list_extend(opts.file_types, { "Avante" })
+        end,
+      },
+    },
+  },
+}
