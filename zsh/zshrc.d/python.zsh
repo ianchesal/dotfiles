6c3d2831ef4e2531565c03ecc37bb7b17ff0d50e
Ian Chesal <ian@withpersona.com>
2025-03-25 19:14:21 -0400

    Revert "Guard some initialization stuff"
    This reverts commit 06b9807687845b390d9f1acaaaf5dbed81c262e5.

--- zsh/zshrc.d/python.zsh
+++ zsh/zshrc.d/python.zsh
@@ -10,37 +10,34 @@ export PIPENV_IGNORE_VIRTUALENVS=1
 if ! type pyenv >/dev/null; then
   path+="$PYENV_ROOT/bin"
 fi
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
 
-if type pyenv >/dev/null; then
-  eval "$(pyenv init --path)"
-  eval "$(pyenv init -)"
+# Pipenv completion
+_pipenv() {
+  eval $(env COMMANDLINE="${words[1, $CURRENT]}" _PIPENV_COMPLETE=complete-zsh pipenv)
+}
+compdef _pipenv pipenv
 
-  # Pipenv completion
-  _pipenv() {
-    eval $(env COMMANDLINE="${words[1, $CURRENT]}" _PIPENV_COMPLETE=complete-zsh pipenv)
-  }
-  compdef _pipenv pipenv
-
-  # Automatic pipenv shell activation/deactivation
-  _togglePipenvShell() {
-    # deactivate shell if Pipfile doesn't exist and not in a subdir
-    if [[ ! -f "$PWD/Pipfile" ]]; then
-      if [[ "$PIPENV_ACTIVE" == 1 ]]; then
-        if [[ "$PWD" != "$pipfile_dir"* ]]; then
-          exit
-        fi
+# Automatic pipenv shell activation/deactivation
+_togglePipenvShell() {
+  # deactivate shell if Pipfile doesn't exist and not in a subdir
+  if [[ ! -f "$PWD/Pipfile" ]]; then
+    if [[ "$PIPENV_ACTIVE" == 1 ]]; then
+      if [[ "$PWD" != "$pipfile_dir"* ]]; then
+        exit
       fi
     fi
+  fi
 
-    # activate the shell if Pipfile exists
-    if [[ "$PIPENV_ACTIVE" != 1 ]]; then
-      if [[ -f "$PWD/Pipfile" ]]; then
-        export pipfile_dir="$PWD"
-        pipenv shell
-      fi
+  # activate the shell if Pipfile exists
+  if [[ "$PIPENV_ACTIVE" != 1 ]]; then
+    if [[ -f "$PWD/Pipfile" ]]; then
+      export pipfile_dir="$PWD"
+      pipenv shell
     fi
-  }
-  autoload -U add-zsh-hook
-  add-zsh-hook chpwd _togglePipenvShell
-  _togglePipenvShell
-fi
+  fi
+}
+autoload -U add-zsh-hook
+add-zsh-hook chpwd _togglePipenvShell
+_togglePipenvShell
