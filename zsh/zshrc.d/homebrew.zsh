7ecf28c5212468166ee4691e256e4425036f016c
Ian Chesal <ian@withpersona.com>
2025-03-25 19:25:28 -0400

    Don't init Homebrew if Homebrew isn't there

--- zsh/zshrc.d/homebrew.zsh
+++ zsh/zshrc.d/homebrew.zsh
@@ -1,16 +1,18 @@
-# For the 5.0.x Homebrew'ed zsh installation...
-# unalias run-help
-autoload run-help
-HELPDIR=${HOMEBREW_PREFIX}/share/zsh/help
+if type brew &>/dev/null; then
+  # For the 5.0.x Homebrew'ed zsh installation...
+  # unalias run-help
+  autoload run-help
+  HELPDIR=${HOMEBREW_PREFIX}/share/zsh/help
 
-# Opt out of Homebrew analytics
-export HOMEBREW_NO_ANALYTICS=1
+  # Opt out of Homebrew analytics
+  export HOMEBREW_NO_ANALYTICS=1
 
-# Cask options
-HOMEBREW_CASK_OPTS="--appdir=~/Applications"
+  # Cask options
+  HOMEBREW_CASK_OPTS="--appdir=~/Applications"
 
-# Gnu Util installed in homebrew?
-GNUTILS_PATH="$(brew --prefix)/opt/grep/libexec/gnubin"
-if [ -d "$GNUTILS_PATH" ]; then
-  path=($GNUTILS_PATH "$path[@]")
+  # Gnu Util installed in homebrew?
+  GNUTILS_PATH="$(brew --prefix)/opt/grep/libexec/gnubin"
+  if [ -d "$GNUTILS_PATH" ]; then
+    path=($GNUTILS_PATH "$path[@]")
+  fi
 fi
