0fdb75dd3e839e3b094dd1a9b444c78678b44c18
Ian Chesal <ian.chesal@gmail.com>
2025-03-28 08:28:21 -0400

    Trying to fix the annoying thing with blink
    HOLY HELL! I think this fixed it. The thing where hitting enter at the
    end of a line caused blink to select the top of the autocompletion list
    is now NO MORE!
    
    Thank you AstroVim for showing me the way here.
    
    I might not even need the keymaps, though they make sense to my brain. I
    think it was just the bit in the `list` section that fixed this.
    Specifically the `preselect = false` part.

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -13,5 +13,42 @@ return {
 
       return not disabled
     end,
+    -- Stolen from AstroVim
+    completion = {
+      list = { selection = { preselect = false, auto_insert = true } },
+    },
+    keymap = {
+      ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<C-N>"] = { "select_next", "show" },
+      ["<C-P>"] = { "select_prev", "show" },
+      ["<C-J>"] = { "select_next", "fallback" },
+      ["<C-K>"] = { "select_prev", "fallback" },
+      ["<C-U>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-D>"] = { "scroll_documentation_down", "fallback" },
+      ["<C-e>"] = { "hide", "fallback" },
+      ["<CR>"] = { "accept", "fallback" },
+      ["<Tab>"] = {
+        "snippet_forward",
+        "select_next",
+        function(cmp)
+          if has_words_before() or vim.api.nvim_get_mode().mode == "c" then
+            return cmp.show()
+          end
+        end,
+        "fallback",
+      },
+      ["<S-Tab>"] = {
+        "snippet_backward",
+        "select_prev",
+        function(cmp)
+          if vim.api.nvim_get_mode().mode == "c" then
+            return cmp.show()
+          end
+        end,
+        "fallback",
+      },
+    },
   },
 }
