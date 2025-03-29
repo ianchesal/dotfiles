4a5753037c296d16c20e00fcd989fec2c74410a5
Ian Chesal <ian.chesal@gmail.com>
2025-03-29 19:52:11 -0400

    Don't use this package
    I don't think it matters

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -73,7 +73,7 @@ return {
     build = "make",
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
+      -- "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       {
