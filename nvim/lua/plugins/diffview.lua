8d6448eb31ba923f558a6bf136311877396938fe
Ian Chesal <ian.chesal@gmail.com>
2025-04-09 09:30:33 -0400

    Clean up diffview and neogit configs
    Wow did I have these wrong! So much nicer.

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -2,8 +2,7 @@ return {
   "sindrets/diffview.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    -- I'm not using neogit
-    -- { "TimUntersberger/neogit", config = { disable_commit_confirmation = true } },
+    "NeogitOrg/neogit",
   },
   -- commit = "9359f7b1dd3cb9fb1e020f57a91f8547be3558c6", -- HEAD requires git 2.31
   -- Keymaps are in ../config/keymaps.lua
