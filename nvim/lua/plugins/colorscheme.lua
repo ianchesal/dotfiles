e83fe249466c58928172444d6a6728ee2d5fb189
Ian Chesal <ian.chesal@gmail.com>
2025-04-22 06:48:56 -0400

    Try out some new color schemes in vim
    Why not?

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,24 @@
 return {
   {
     "navarasu/onedark.nvim",
-    lazy = true,
+    name = "onedark",
+    lazy = false,
     priority = 1000,
-    opts = {
-      style = "deep",
-      lualine = {
-        transparent = true,
-      },
-    },
+    opts = { style = "deep", lualine = { transparent = true } },
   },
-
+  { "EdenEast/nightfox.nvim", name = "nightfox", lazy = false, priority = 1000 },
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+  { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
+  { "projekt0n/github-nvim-theme", name = "github-theme", lazy = false, priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "catppuccin",
-      colorscheme = "tokyonight-night",
+      -- colorscheme = "github_dark_default",
       -- colorscheme = "onedark",
       -- colorscheme = "quiet",
+      -- colorscheme = "tokyonight-night",
+      colorscheme = "moonfly",
     },
   },
 }
