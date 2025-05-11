0c6c540c43c48c30c5506a76607e06c4f229a0ae
Ian Chesal <ian.chesal@gmail.com>
2025-05-11 09:37:47 -0400

    Fix for Mason v2 in LazyVim
    Comes from https://github.com/LazyVim/LazyVim/pull/6053#issuecomment-2865604700
    
    This is a bit of a trust fail exercise here. But the person seems on the
    up and up.
    
    Will revert once u/folke gets back from vacation and incorporates this
    in to the LazyVim main branch.

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,7 +1,17 @@
 return {
   {
+    -- https://github.com/LazyVim/LazyVim/pull/6053#issuecomment-2865604700
+    -- Fixes the LazyVim/Mason 2.0 incompatibility
+    -- Will dump this once u/folke has an official fix after he gets back
+    -- from his well-deserved vacation.
+    "LazyVim/LazyVim",
+    url = "https://github.com/dpetka2001/LazyVim",
+    branch = "fix/mason-v2",
+  },
+  {
     "mason-org/mason.nvim",
-    version = "^1.0.0",
+    -- No need to pin to <v2 with the above fix
+    -- version = "^1.0.0",
     opts = {
       ensure_installed = {
         "bash-language-server",
@@ -27,8 +37,8 @@ return {
       },
     },
   },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    version = "^1.0.0",
-  },
+  -- {
+  --   "mason-org/mason-lspconfig.nvim",
+  --   version = "^1.0.0",
+  -- },
 }
