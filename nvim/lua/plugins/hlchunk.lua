f29009e24af7e7a8aa3793c65579da7ce333132e
Ian Chesal <ian.chesal@gmail.com>
2025-05-23 11:51:58 -0400

    Switch to hlchunk
    Prompted by this r/neovim post:
    
    https://old.reddit.com/r/neovim/comments/1ktiexh/which_plugin_shows_this_curved_blue_line_for/
    
    This is so much more information than what mini.indentscope or snacks
    give for indentation. For Ruby this'll be GREAT!

---
+++ nvim/lua/plugins/hlchunk.lua
@@ -0,0 +1,18 @@
+return {
+  "shellRaining/hlchunk.nvim", -- indent-blankline.nvim alternative
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("hlchunk").setup({
+      chunk = {
+        enable = true,
+        chars = { right_arrow = "─" },
+        style = "#75A1FF",
+        duration = 50,
+        delay = 10,
+      },
+      indent = { enable = true },
+      line_num = { enable = true },
+      exclude_filetypes = { "help", "git", "markdown", "snippets", "text", "gitconfig", "alpha", "dashboard" },
+    })
+  end,
+}
