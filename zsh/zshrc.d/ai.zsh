5c691337277ab90f040977b4ae2f6e60d3ad49b3
Ian Chesal <ian@withpersona.com>
2025-06-25 12:12:37 -0400

    Store Gemini settings in my dotfiles
    These look safe to keep in git for now.

--- zsh/zshrc.d/ai.zsh
+++ zsh/zshrc.d/ai.zsh
@@ -5,5 +5,5 @@ CLAUDE_BASH_MAINTAIN_PROJECT_WORKING_DIR=true
 
 ## Google Gemini
 function gemini() {
-  GOOGLE_CLOUD_PROJECT=${GEMINI_CLOUD_PROJECT} command gemini "$@"
+  GOOGLE_CLOUD_PROJECT=${GEMINI_CLOUD_PROJECT} ~/.npm-global/bin/gemini "$@"
 }
