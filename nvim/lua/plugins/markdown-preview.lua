e929fcba7dc2fdb5f4ae6eb4cd195e9e4ca2a7bc
Ian Chesal <ian@withpersona.com>
2025-09-16 19:51:47 -0400

    Bring back MarkdownPreview
    This is a useful plugin. I just don't want all the other stuff that
    comes along with the LazyVim Markdown extra.

---
+++ nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,21 @@
+-- I want this plugin but I don't want all the other cruft that
+-- comes in with using LazyVim's lang.markdown extra.
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = function()
+    require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+    vim.fn["mkdp#util#install"]()
+  end,
+  keys = {
+    {
+      "<leader>cp",
+      ft = "markdown",
+      "<cmd>MarkdownPreviewToggle<cr>",
+      desc = "Markdown Preview",
+    },
+  },
+  config = function()
+    vim.cmd([[do FileType]])
+  end,
+}
