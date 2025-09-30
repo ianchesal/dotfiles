8456536ff5070856c5000d4ad0baf96ff8d87035
Ian Chesal <ian@withpersona.com>
2025-09-30 13:38:31 -0400

    Make shell compatible with sidekick.nvim
    It doesn't like shell functions. That's fine. I'm not doing the
    double-launch thing any more anyways.

--- zsh/zshrc.d/ai.zsh
+++ zsh/zshrc.d/ai.zsh
@@ -2,8 +2,3 @@
 
 ## Anthropic
 CLAUDE_BASH_MAINTAIN_PROJECT_WORKING_DIR=true
-
-## Google Gemini
-function gemini() {
-  GOOGLE_CLOUD_PROJECT=${GEMINI_CLOUD_PROJECT} ~/.npm-global/bin/gemini "$@"
-}
