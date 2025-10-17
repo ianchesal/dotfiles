af3e35e0b79b23017e9e8f595eb922ecf21f6899
Ian Chesal <ian@withpersona.com>
2025-10-17 11:32:07 -0400

    Improve this script so it like actually works

--- brew/brew-install.sh
+++ brew/brew-install.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
+set -e
+
+# Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew tap Homebrew/bundle
+
+# Detect brew location and add to PATH for this session
+if [[ -x /opt/homebrew/bin/brew ]]; then
+  # macOS Apple Silicon
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [[ -x /usr/local/bin/brew ]]; then
+  # macOS Intel
+  eval "$(/usr/local/bin/brew shellenv)"
+elif [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+  # Linux
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+else
+  echo "Error: Could not find brew installation" >&2
+  exit 1
+fi
+
+# Now brew should be in PATH, run bundle
 cd "$(dirname "$0")"
 brew bundle
