5cbd230c5b0eed593685192379eab4511d08ac0c
Ian Chesal <ian.chesal@gmail.com>
2025-11-10 20:57:34 -0500

    Switch to lazygit
    Go back to lazygit in Neovim
    
    Maybe I like it?

--- nvim/lua/plugins/diffview.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "sindrets/diffview.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "NeogitOrg/neogit",
-  },
-  -- commit = "9359f7b1dd3cb9fb1e020f57a91f8547be3558c6", -- HEAD requires git 2.31
-  -- Keymaps are in ../config/keymaps.lua
-}
