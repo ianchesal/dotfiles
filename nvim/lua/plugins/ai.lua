688e36a5259ed71833c1cdb39baeb944a03a3f40
Ian Chesal <ian@withpersona.com>
2026-01-12 17:39:43 -0500

    New AI plugin for working with Claude via tmux conduits
    Works pretty well but it's bare bones right now. Will help improve it.

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,30 +1,31 @@
 return {
-  {
-    "kitallen23/conduit.nvim",
-    config = function()
-      local conduit = require("conduit")
-      local keymap = vim.keymap
-      -- vim.g.conduit_opts = {
-      --   -- Put your options here
-      -- }
-
-      keymap.set("n", "<leader>ai", function()
-        conduit.ask()
-      end, { desc = "Generate conduit prompt" })
-      keymap.set("n", "<leader>ac", function()
-        conduit.ask("@cursor: ")
-      end, { desc = "Generate conduit prompt at cursor" })
-      keymap.set("v", "<leader>ai", function()
-        conduit.ask("@selection: ")
-      end, { desc = "Generate conduit prompt about selection" })
-      keymap.set({ "n", "v" }, "<leader>ap", function()
-        conduit.select_prompt()
-      end, { desc = "Select conduit prompt" })
-      keymap.set("n", "<leader>ad", function()
-        conduit.select_prompt("fix_line")
-      end, { desc = "Get diagnostic prompt" })
-    end,
-  },
+  -- {
+  --   "kitallen23/conduit.nvim",
+  --   config = function()
+  --     local conduit = require("conduit")
+  --     local keymap = vim.keymap
+  --     -- vim.g.conduit_opts = {
+  --     --   -- Put your options here
+  --     -- }
+  --
+  --     keymap.set("n", "<leader>ai", function()
+  --       conduit.ask()
+  --     end, { desc = "Generate conduit prompt" })
+  --     keymap.set("n", "<leader>ac", function()
+  --       conduit.ask("@cursor: ")
+  --     end, { desc = "Generate conduit prompt at cursor" })
+  --     keymap.set("v", "<leader>ai", function()
+  --       conduit.ask("@selection: ")
+  --     end, { desc = "Generate conduit prompt about selection" })
+  --     keymap.set({ "n", "v" }, "<leader>ap", function()
+  --       conduit.select_prompt()
+  --     end, { desc = "Select conduit prompt" })
+  --     keymap.set("n", "<leader>ad", function()
+  --       conduit.select_prompt("fix_line")
+  --     end, { desc = "Get diagnostic prompt" })
+  --   end,
+  -- },
+  { "andreypopp/cctools" },
   {
     "jim-at-jibba/nvim-redraft",
     dependencies = {
