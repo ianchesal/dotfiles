96fcaef8a70fa400c0cf1e1bf183d8755874cecf
Ian Chesal <ian@withpersona.com>
2026-03-29 09:49:37 -0400

    Bring back Diffview -- it's useful

---
+++ nvim/lua/plugins/diffview.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    'sindrets/diffview.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewToggleFiles', 'DiffviewFocusFiles' },
+  },
+}
